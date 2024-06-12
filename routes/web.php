70594be3ace2b9f6f0f170df081462a192ca6342
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-06-12 13:01:35 +0500

    jv2 print completed

--- routes/web.php
+++ routes/web.php
@@ -85,8 +85,12 @@ Route::get('/vouchers/jv2/new', [App\Http\Controllers\JV2Controller::class, 'cre
 Route::post('/vouchers/jv2/create', [App\Http\Controllers\JV2Controller::class, 'store'])->name('store-jv2');
 Route::get('/vouchers/jv2/edit/{id}', [App\Http\Controllers\JV2Controller::class, 'edit'])->name('edit-jv2');
 Route::post('/vouchers/jv2/update', [App\Http\Controllers\JV2Controller::class, 'update'])->name('update-jv2');
+Route::post('/vouchers/jv2/delete', [App\Http\Controllers\JV2Controller::class, 'destroy'])->name('delete-jv2');
 Route::get('/vouchers/jv2/print/{id}', [App\Http\Controllers\JV2Controller::class, 'print'])->name('print-jv2');
 Route::get('/vouchers/jv2/attachements', [App\Http\Controllers\JV2Controller::class, 'getAttachements'])->name('get-jv2-att');
+Route::get('/vouchers/jv2/download/{id}', [App\Http\Controllers\JV2Controller::class, 'downloadAtt'])->name('jv2-att-download');
+Route::get('/vouchers/jv2/view/{id}', [App\Http\Controllers\JV2Controller::class, 'view'])->name('jv2-att-view');
+Route::delete('/vouchers/jv2/deleteAttachment/{id}', [App\Http\Controllers\JV2Controller::class, 'deleteAtt'])->name('jv2-att-delete');
 
 //sales
 Route::get('/sales/all-invoices', [App\Http\Controllers\SalesController::class, 'index'])->name('all-saleinvoices');
