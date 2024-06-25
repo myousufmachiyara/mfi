54d504883ee44dd59f53d5e328ffa47560542d34
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-06-25 08:24:18 +0500

    coa heading fix

--- resources/views/ac/index.blade.php
+++ resources/views/ac/index.blade.php
@@ -9,7 +9,7 @@
                         <div class="col">
                             <section class="card">
                                 <header class="card-header">
-                                    <h2 class="card-title">Chart Of Accounts</h2>
+                                    <h2 class="card-title mb-2">Chart Of Accounts</h2>
                                     <div class="card-actions">
                                         <button type="button" class="modal-with-form btn btn-primary" href="#addModal"> <i class="fas fa-plus">  </i>  New Account</button>
                                         <button type="button" class="btn btn-danger" onclick="printReport()"> <i class="fas fa-file-pdf">  </i>  Print Report</button>
