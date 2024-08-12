ca1ff5abb7f5ae84b21a95692f3da76011ac62af
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-08-12 08:03:55 +0500

    restructured menu

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -8,15 +8,12 @@
                     <div class="row">
                         <div class="col">
                             <section class="card">
-                                <header class="card-header">
-                                    <div class="row">
-                                    <div class="col-6">
-                                        <h2 class="card-title">Items</h2>
-                                    </div>
-
-                                    <div class="card-actions col-6 text-end">
-                                        <button type="button" class="btn btn-primary mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal" href="#createModal"> <i class="fas fa-plus" ></i> New Item</button>
-                                    </div>
+                                <header class="card-header" style="display: flex;justify-content: space-between;">
+                                    <h2 class="card-title">All Items</h2>
+                                    <form class="text-end" action="{{ route('create-item') }}" method="GET">
+                                        <button type="button" class="btn btn-primary mt-2 modal-with-zoom-anim ws-normal" href="#createModal"> <i class="fas fa-plus" ></i> Add Single Item</button>
+                                        <button type="submit" class="btn btn-primary mt-2"> <i class="fas fa-plus"></i> Add Multiple Items</button>
+                                    </form>
                                 </header>
                                 <div class="card-body">
                                 	<table class="table table-bordered table-striped mb-0" id="datatable-default">
