9b7a5864833de7abe115e312cc2601dcf36ada5d
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-08-23 15:32:03 +0500

    mobile menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -214,7 +214,7 @@
 	</div>
 
 	<!-- start: search & user box -->
-	<div class="header-right">	
+	<div class="header-right d-sm-none">	
 		<span class="separator"></span>
 
 		<div id="userbox" class="userbox">
