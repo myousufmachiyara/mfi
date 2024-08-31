92870279ce4fba224ef73e16780bec60c21e05f9
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-08-31 13:29:51 +0500

    Update JV2Controller.php

--- app/Http/Controllers/JV2Controller.php
+++ app/Http/Controllers/JV2Controller.php
@@ -129,9 +129,9 @@ class JV2Controller extends Controller
                     $pur_ageing = new purchase_ageing();
 
                     $pur_ageing->jv2_id=$latest_jv2['jv_no'];
-                    $pur_ageing->amount=$request->rec_amount[$k];
-                    $pur_ageing->sales_id=$request->invoice_nos[$k];
-                    $pur_ageing->sales_prefix=$request->prefix[$k];
+                    $pur_ageing->amount=$request->pur_rec_amount[$k];
+                    $pur_ageing->sales_id=$request->pur_invoice_nos[$k];
+                    $pur_ageing->sales_prefix=$request->pur_prefix[$k];
                     $pur_ageing->acc_name=$request->customer_name;
                     $pur_ageing->created_by=1;
                     $pur_ageing->save();
