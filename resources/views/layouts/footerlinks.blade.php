4ba9bd0ab5dbd47c55602eaefe3a8d32fb77d29c
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-02 04:15:32 +0500

    css loading test

--- resources/views/layouts/footerlinks.blade.php
+++ resources/views/layouts/footerlinks.blade.php
@@ -1,35 +1,35 @@
+<!-- jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nanoscroller/0.8.7/jquery.nanoscroller.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-placeholder/2.3.1/jquery.placeholder.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.appear/0.4.1/jquery.appear.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
+
 <!-- Vendor -->
-<script src="/assets/vendor/jquery/jquery.js"></script>
-<script src="/assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
-<script src="/assets/vendor/popper/umd/popper.min.js"></script>
 <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
 <script src="/assets/vendor/common/common.js"></script>
-<script src="/assets/vendor/nanoscroller/nanoscroller.js"></script>
-<script src="/assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
-
-<!-- Specific Page Vendor -->
-<script src="/assets/vendor/jquery-ui/jquery-ui.js"></script>
-<script src="/assets/vendor/jqueryui-touch-punch/jquery.ui.touch-punch.js"></script>
 <script src="/assets/vendor/select2/js/select2.js"></script>
-<script src="/assets/vendor/bootstrapv5-multiselect/js/bootstrap-multiselect.js"></script>
-<script src="/assets/vendor/datatables/media/js/jquery.dataTables.min.js"></script>
-<script src="/assets/vendor/datatables/media/js/dataTables.bootstrap5.min.js"></script>
-
 
 <!-- Theme Base, Components and Settings -->
-<!-- <script src="/assets/js/theme.js"></script> -->
+<script src="/assets/js/theme.js"></script>
 
 <!-- Theme Custom -->
 <script src="/assets/js/custom.js"></script>
 
 <!-- Theme Initialization Files -->
-<!-- <script src="/assets/js/theme.init.js"></script> -->
+<script src="/assets/js/theme.init.js"></script>
 
 <!-- Examples -->
-<!-- <script src="/assets/js/examples/examples.header.menu.js"></script>
+<script src="/assets/js/examples/examples.header.menu.js"></script>
 <script src="/assets/js/examples/examples.dashboard.js"></script>
 <script src="/assets/js/examples/examples.datatables.default.js"></script>
-<script src="/assets/js/examples/examples.modals.js"></script> -->
+<script src="/assets/js/examples/examples.modals.js"></script>
 
 
