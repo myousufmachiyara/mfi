359c698ec0eed0b844006470f2ad67294aa22f6f
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-02 15:50:37 +0500

    Update SalesController.php

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -144,7 +144,8 @@ class SalesController extends Controller
                         ->join('item_entry','sales_2.item_cod','=','item_entry.it_cod')
                         ->select('sales_2.*','item_entry.item_name')
                         ->get();
-
+        
+        die(print_r($sale_items));
         return view('sales.view',compact('sales','sale_items'));
     }
 
