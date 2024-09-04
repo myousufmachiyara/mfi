eeac0b4261ad48894b8aabf588bcd240e755a1da
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-04 16:53:18 +0500

    extra att fixed

--- resources/views/tstock_out/index.blade.php
+++ resources/views/tstock_out/index.blade.php
@@ -40,7 +40,7 @@
                                             <tbody>
                                                 @foreach ($tstock_out as $key => $row)
                                                 <tr>
-                                                    <td>{{$row->Sal_inv_no}}</td>
+                                                    <td style="display:none" >{{$row->Sal_inv_no}}</td>
                                                     <td>{{$row->prefix}}{{$row->Sal_inv_no}}</td>
                                                     <td>{{ \Carbon\Carbon::parse($row->sa_date)->format('d-m-y') }}</td>
                                                     <td><strong>{{$row->ac_name}}</strong></td>
