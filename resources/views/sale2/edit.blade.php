07adc5457306b0d287ba32f904eae529da761786
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-05 17:22:43 +0500

    Update edit.blade.php

--- resources/views/sale2/edit.blade.php
+++ resources/views/sale2/edit.blade.php
@@ -219,7 +219,7 @@
 									<footer class="card-footer">
 										<div class="row form-group mb-2">
 											<div class="text-end">
-												<button type="button" class="btn btn-danger mt-2"  onclick="window.location='{{ route('all-purchases2') }}'"> <i class="fas fa-trash"></i> Discard Changes</button>
+												<button type="button" class="btn btn-danger mt-2"  onclick="window.location='{{ route('all-sale2invoices') }}'"> <i class="fas fa-trash"></i> Discard Changes</button>
 												<button type="submit" class="btn btn-primary mt-2"> <i class="fas fa-save"></i> Update Invoice</button>
 											</div>
 										</div>
