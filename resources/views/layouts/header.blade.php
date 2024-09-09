eb54fe718bdefca5b725c8d67181778122345cf6
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-09 15:27:27 +0500

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="fixed js flexbox flexboxlegacy no-touch csstransforms csstransforms3d no-overflowscrolling webkit chrome win js no-mobile-device custom-scroll">
+<html class="fixed ">
 	<head>
 
 		<!-- Basic -->
