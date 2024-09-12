c3a151c7499bd21cc482cfe20a497c652e0325a5
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-12 17:39:26 +0500

    Update index.blade.php

--- resources/views/purchase1/index.blade.php
+++ resources/views/purchase1/index.blade.php
@@ -10,8 +10,8 @@
             border: 16px solid #f3f3f3; /* Light grey */
             border-top: 16px solid #3498db; /* Blue */
             border-radius: 50%;
-            width: 120px;
-            height: 120px;
+            width: 60px;
+            height: 60px;
             animation: spin 2s linear infinite;
         }
         @keyframes spin {
@@ -245,7 +245,7 @@
                 }
             }
         }
-        document.getElementById('loader').style.display = 'none';
+        document.getElementById('searchloader').style.display = 'none';
        
     }
 </script>
