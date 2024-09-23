9f0817b979c59ee9ba80d3a04f0a45f0c2b19c8a
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-24 01:30:49 +0500

    Update index.blade.php

--- resources/views/purchase1/index.blade.php
+++ resources/views/purchase1/index.blade.php
@@ -27,9 +27,9 @@
                         <div class="col">
                             <section class="card">
                                 <header class="card-header" style="display: flex;justify-content: space-between;">
-                                    <h2 class="card-title">All Purchase Invoices</h2>
+                                    <h2 class="card-title">All Purchases</h2>
                                     <form class="text-end" action="{{ route('new-purchases1') }}" method="GET">
-                                        <button type="submit" class="btn btn-primary mt-2"> <i class="fas fa-plus"></i> New Purchase Invoice</button>
+                                        <button type="submit" class="btn btn-primary mt-2"> <i class="fas fa-plus"></i> New Invoice</button>
                                     </form>
                                 </header>
                                
