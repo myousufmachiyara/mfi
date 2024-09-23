656643b7be1fdc36b798d59bde0abe349ba68ced
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-24 03:17:52 +0500

    Update index.blade.php

--- resources/views/sale2/index.blade.php
+++ resources/views/sale2/index.blade.php
@@ -15,7 +15,7 @@
                                 <header class="card-header" style="display: flex;justify-content: space-between;">
                                     <h2 class="card-title">All Sale Pipe</h2>
                                     <form class="text-end" action="{{ route('new-sales2') }}" method="GET">
-                                        <button type="submit" class="btn btn-primary mt-2"> <i class="fas fa-plus"></i> New Invoice</button>
+                                        <button type="submit" class="btn btn-primary"> <i class="fas fa-plus"></i> New Invoice</button>
                                     </form>
                                 </header>
                                 <div class="card-body">
