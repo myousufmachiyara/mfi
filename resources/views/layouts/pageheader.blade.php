72ece4a618bacac69a741162232b20202e34c636
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-23 15:45:17 +0500

    Update pageheader.blade.php

--- resources/views/layouts/pageheader.blade.php
+++ resources/views/layouts/pageheader.blade.php
@@ -2,44 +2,45 @@
 	<div></div>
 </div>
 <header class="header header-nav-menu header-nav-top-line ">
-
+	
 	<div class="logo-container">
 		<a href="/home" class="logo">						
 			<img src="/assets/img/logo.png" width="55" height="35" alt="MFI Logo" />
 		</a>	
-		<div class="header-right">	
-			<span class="separator"></span>
-			<div id="userbox" class="userbox">
-				<a href="#" data-bs-toggle="dropdown">
-					<div class="profile-info">
-						<span class="name">{{session('user_name')}}</span>
-						<span class="role">{{session('role_name')}}</span>
-					</div>
 
-					<i class="fa custom-caret"></i>
-				</a>
-
-				<div class="dropdown-menu">
-					<ul class="list-unstyled">
-						<li class="divider"></li>
-						<!-- <li>
-							<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="bx bx-lock"></i> Lock Screen</a>
-						</li> -->
-						<li>
-							<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-user"></i> Profile</a>
-						</li>
-						<li>
-							<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-lock"></i> Reset Password</a>
-						</li>
-						<li>
-							<a role="menuitem" tabindex="-1" href="/logout"><i class="bx bx-power-off"></i> Logout</a>
-						</li>
-					</ul>
+		<div id="userbox" class="userbox">
+			<a href="#" data-bs-toggle="dropdown">
+				<div class="profile-info">
+					<span class="name">{{session('user_name')}}</span>
+					<span class="role">{{session('role_name')}}</span>
 				</div>
+
+				<i class="fa custom-caret"></i>
+			</a>
+
+			<div class="dropdown-menu">
+				<ul class="list-unstyled">
+					<li class="divider"></li>
+					<!-- <li>
+						<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="bx bx-lock"></i> Lock Screen</a>
+					</li> -->
+					<li>
+						<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-user"></i> Profile</a>
+					</li>
+					<li>
+						<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-lock"></i> Reset Password</a>
+					</li>
+					<li>
+						<a role="menuitem" tabindex="-1" href="/logout"><i class="bx bx-power-off"></i> Logout</a>
+					</li>
+				</ul>
 			</div>
 		</div>				
 	</div>
 
-
+	<!-- <div class="header-right">	
+		<span class="separator"></span>
+		
+	</div> -->
 
 </header>
