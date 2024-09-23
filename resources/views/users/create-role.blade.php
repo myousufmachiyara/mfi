005e42bcc0339c94bf02e8d035dc3d8c817e9f98
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-24 00:38:19 +0500

    Update create-role.blade.php

--- resources/views/users/create-role.blade.php
+++ resources/views/users/create-role.blade.php
@@ -1,7 +1,7 @@
 @include('../layouts.header')
 	<body>
 		<section class="body">
-        <!-- @include('../layouts.pageheader') -->
+        @include('../layouts.pageheader')
             <div class="inner-wrapper">
 				<section role="main" class="content-body">
 					<form method="post" action="{{ route('create-role') }}" enctype="multipart/form-data" onkeydown="return event.key != 'Enter';" id="addForm">
