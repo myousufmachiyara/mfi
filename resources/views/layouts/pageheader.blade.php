00e45329fceef4f5be5d6a26cfdccc3a95003732
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-23 13:29:20 +0500

    Update pageheader.blade.php

--- resources/views/layouts/pageheader.blade.php
+++ resources/views/layouts/pageheader.blade.php
@@ -9,38 +9,40 @@
 		<div class="d-md-none toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">						
 			<i class="fas fa-bars" aria-label="Toggle sidebar" style="line-height:2.3"></i>					
 		</div>
-		<div class="header-right">	
-			<span class="separator"></span>
+	</div>
 
-			<div id="userbox" class="userbox">
-				<a href="#" data-bs-toggle="dropdown">
-					
-					<div class="profile-info">
-						<span class="name">{{session('user_name')}}</span>
-						<span class="role">{{session('role_name')}}</span>
-					</div>
+	<div class="header-right">	
+		<span class="separator"></span>
 
-					<i class="fa custom-caret"></i>
-				</a>
-
-				<div class="dropdown-menu">
-					<ul class="list-unstyled">
-						<li class="divider"></li>
-						<!-- <li>
-							<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="bx bx-lock"></i> Lock Screen</a>
-						</li> -->
-						<li>
-							<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-user"></i> Profile</a>
-						</li>
-						<li>
-							<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-lock"></i> Reset Password</a>
-						</li>
-						<li>
-							<a role="menuitem" tabindex="-1" href="/logout"><i class="bx bx-power-off"></i> Logout</a>
-						</li>
-					</ul>
+		<div id="userbox" class="userbox">
+			<a href="#" data-bs-toggle="dropdown">
+				
+				<div class="profile-info">
+					<span class="name">{{session('user_name')}}</span>
+					<span class="role">{{session('role_name')}}</span>
 				</div>
+
+				<i class="fa custom-caret"></i>
+			</a>
+
+			<div class="dropdown-menu">
+				<ul class="list-unstyled">
+					<li class="divider"></li>
+					<!-- <li>
+						<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="bx bx-lock"></i> Lock Screen</a>
+					</li> -->
+					<li>
+						<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-user"></i> Profile</a>
+					</li>
+					<li>
+						<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-lock"></i> Reset Password</a>
+					</li>
+					<li>
+						<a role="menuitem" tabindex="-1" href="/logout"><i class="bx bx-power-off"></i> Logout</a>
+					</li>
+				</ul>
 			</div>
 		</div>
-	</div>	
+	</div>
+	
 </header>
