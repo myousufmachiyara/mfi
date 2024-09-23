58a32502c1ffadad2232bcf1a227d3c5e4aecc13
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-09-23 15:56:53 +0500

    Update pageheader.blade.php

--- resources/views/layouts/pageheader.blade.php
+++ resources/views/layouts/pageheader.blade.php
@@ -4,11 +4,10 @@
 <header class="header header-nav-menu header-nav-top-line ">
 	
 	<div class="logo-container">
-		<a href="/home" class="logo">						
+		<a href="/home" class="logo" style="float:left !important">						
 			<img src="/assets/img/logo.png" width="55" height="35" alt="MFI Logo" />
 		</a>	
-
-		<div id="userbox" class="userbox header-right">
+		<div id="userbox" class="userbox" style="float:right !important;margin:16px 10px 0 0px">
 			<a href="#" data-bs-toggle="dropdown">
 				<div class="profile-info">
 					<span class="name">{{session('user_name')}}</span>
@@ -18,12 +17,8 @@
 				<i class="fa custom-caret"></i>
 			</a>
 
-			<div class="dropdown-menu">
+			<div class="dropdown-menu" >
 				<ul class="list-unstyled">
-					<li class="divider"></li>
-					<!-- <li>
-						<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="bx bx-lock"></i> Lock Screen</a>
-					</li> -->
 					<li>
 						<a role="menuitem" tabindex="-1" href="#"><i class="bx bx-user"></i> Profile</a>
 					</li>
@@ -37,10 +32,5 @@
 			</div>
 		</div>				
 	</div>
-
-	<!-- <div class="header-right">	
-		<span class="separator"></span>
-		
-	</div> -->
 
 </header>
