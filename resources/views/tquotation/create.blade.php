6138803570cafed8eff8572ab03bb493e049b784
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-10-01 00:54:55 +0500

    Update create.blade.php

--- resources/views/tquotation/create.blade.php
+++ resources/views/tquotation/create.blade.php
@@ -360,8 +360,7 @@
 					type: "GET",
 					url: "/tquotation/getavailablestock/"+item_id,
 					success: function(result){
-						console.log(result);
-						$('#stock'+row_no).val(result['opp_bal']);
+						$('#stock'+row_no).val(result[0]['opp_bal']);
 					},
 					error: function(){
 						alert("error");
