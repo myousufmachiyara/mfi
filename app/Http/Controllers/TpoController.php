c0142720da28e8ce44706afe916a849e06e7d015
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-10-02 19:29:40 +0500

    s

--- app/Http/Controllers/TpoController.php
+++ app/Http/Controllers/TpoController.php
@@ -498,9 +498,9 @@ public function edit($id)
             
             // Display the totals for the group
             $html .= '<tr>';
-            $html .= '<td colspan="3" style="text-align:right;font-weight:bold">Sub Total===>>:</td>';
-            $html .= '<td style="text-align:center">' . $group_total_quantity . '</td>';
-            $html .= '<td colspan="4" style="text-align:right;font-weight:bold">' . $group_total_amount . '</td>';
+            $html .= '<td colspan="3" style="text-align:right;font-weight:bold;color:#17365D">Sub Total===>></td>';
+            $html .= '<td style="text-align:center;font-weight:bold;color:red">' . $group_total_quantity . '</td>';
+            $html .= '<td colspan="4" style="text-align:center;font-weight:bold;color:red">' . $group_total_amount . '</td>';
             $html .= '</tr>';
             
             // Add the group totals to the overall totals
