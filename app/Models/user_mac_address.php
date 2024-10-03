227d2464112ed27a6168543f84fe80493a5595cf
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-10-03 09:43:07 +0500

    Update user_mac_address.php

--- app/Models/user_mac_address.php
+++ app/Models/user_mac_address.php
@@ -10,6 +10,6 @@ class user_mac_address extends Model
     use HasFactory;
     protected $table = "user_mac_address";
     protected $fillable = [
-        'user_id'
+        'user_id','device_name', 'mac_address',
     ];
 }
