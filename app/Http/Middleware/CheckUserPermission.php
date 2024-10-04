8d4c517bc6b713ba010741ebd542261d235da97c
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-10-04 05:09:46 +0500

    Update CheckUserPermission.php

--- app/Http/Middleware/CheckUserPermission.php
+++ app/Http/Middleware/CheckUserPermission.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 use App\Models\modules;
 use App\Models\role_access;
+use Illuminate\Support\Facades\Session;
 
 class CheckUserPermission
 {
