1534ef5a82ee37e131636a4025923a9a6c9e9b7a
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-10-07 19:35:41 +0500

    g

--- resources/views/weight/create.blade.php
+++ resources/views/weight/create.blade.php
@@ -4,13 +4,13 @@
 		@include('../layouts.pageheader')
 		<div class="inner-wrapper">
 				<section role="main" class="content-body">
-					<form method="post" action="{{ route('store-tquotation') }}" enctype="multipart/form-data" onkeydown="return event.key != 'Enter';" id="addForm">
+					<form method="post" action="{{ route('store-weight') }}" enctype="multipart/form-data" onkeydown="return event.key != 'Enter';" id="addForm">
 						@csrf
 						<div class="row">	
 							<div class="col-12 mb-3">								
 								<section class="card">
 									<header class="card-header" style="display: flex;justify-content: space-between;">
-										<h2 class="card-title">New Quotation Pipes/Garders</h2>
+										<h2 class="card-title">New Weight Calculation</h2>
 										<div class="card-actions">
 											<button type="button" class="btn btn-primary" onclick="addNewRow_btn()"> <i class="fas fa-plus"></i> Add New Row </button>
 										</div>
@@ -19,16 +19,16 @@
 									<div class="card-body">
 										<div class="row form-group mb-2">
 											<div class="col-6 col-md-2 mb-2">
-												<label class="col-form-label" >Quotation No.</label>
+												<label class="col-form-label" >Weight No.</label>
 												<input type="text" placeholder="(New Quotation)" class="form-control" disabled>
 												<input type="hidden" id="itemCount" name="items" value="1" class="form-control">
 											</div>
 											<div class="col-6 col-md-2 mb-2">
 												<label class="col-form-label" >Date</label>
-												<input type="date" name="sa_date" autofocus value="<?php echo date('Y-m-d'); ?>" class="form-control">
+												<input type="date" name="sa_date"  value="<?php echo date('Y-m-d'); ?>" class="form-control">
 											</div>
 											
-											<div class="col-sm-12 col-md-4 mb-3">
+											<div class="col-sm-12 col-md-3 mb-3">
 												<label class="col-form-label">Custmer Name <span style="color: red;">*</span></label>
 												<select data-plugin-selecttwo class="form-control select2-js"  name="account_name" required>
 													<option value="" disabled selected>Select Custmer Name</option>
@@ -37,16 +37,12 @@
 													@endforeach
 												</select>
 											</div>
-											<div class="col-6 col-md-2 mb-2">
-												<label class="col-form-label" >PO#</label>
-												<input type="text" placeholder="PO#" name="pur_ord_no" class="form-control">
-											</div>
 											
-											<div class="col-6 col-md-2 mb-2">
-												<label class="col-form-label" >Sale Inv. No.</label>
-												<input type="text" placeholder="Sale Inv. No." name="sal_inv_no" disabled class="form-control">
+											<div class="col-sm-3 col-md-2 mb-2">
+												<label class="col-form-label" >Name of Person</label>
+												<input type="text" placeholder="Name of Person" name="Cash_pur_name" class="form-control">
 											</div>
-											<div class="col-sm-12 col-md-4 mb-3">
+											<div class="col-sm-12 col-md-3 mb-3">
 												<label class="col-form-label">Dispatch From<span style="color: red;">*</span></label>
 												<select data-plugin-selecttwo class="form-control select2-js"  name="disp_account_name" required>
 													<option value="" disabled selected>Select Dispatch From</option>
@@ -55,15 +51,13 @@
 													@endforeach
 												</select>
 											</div>
-											<div class="col-sm-3 col-md-2 mb-2">
-												<label class="col-form-label" >Name of Person</label>
-												<input type="text" placeholder="Name of Person" name="Cash_pur_name" class="form-control">
-											</div>
 
 											<div class="col-sm-3 col-md-2 mb-2">
 												<label class="col-form-label" >Person Address</label>
 												<input type="text" placeholder="Person Address" name="cash_Pur_address" class="form-control">
 											</div>
+
+											
 											<div class="col-sm-12 col-md-4 mb-3">
 												<label class="col-form-label">Attachements</label>
 												<input type="file" class="form-control" name="att[]" multiple accept=".zip, appliation/zip, application/pdf, image/png, image/jpeg">
@@ -71,22 +65,10 @@
 
 											<div class="col-12 col-md-6 mb-12">
 												<label class="col-form-label">Remarks</label>
-												<textarea rows="4" cols="50" name="Sales_Remarks" id="Sales_Remarks" placeholder="Remarks" class="form-control cust-textarea"></textarea>
+												<textarea rows="2" cols="50" name="Sales_Remarks" id="Sales_Remarks" placeholder="Remarks" class="form-control cust-textarea"></textarea>
 											</div>
 
-											<div class="col-6 mb-2">
-												<label class="col-form-label">Terms And Conditions</label>
-												<textarea rows="4" cols="50" name="tc" id="tc" placeholder="Terms And Conditions" class="form-control cust-textarea">
-* This Quotation is valid for 2 days from the date issued. After this period, a new quotation may be required.
-* The quotation includes only the services/products explicitly stated. Any additional services/products will be subject to further discussion and agreement.
-* Prices quoted are subject to change based on market shift, making this quotation void without any notice. All prices are exclusive of applicable taxes unless stated otherwise.(No additional taxes will be deducted from this amount)
-* Payment terms will be outlined in the final agreement. A deposit of mutually agreed percentage may be required upon acceptance of the quotation.
-* Delivery dates provided are estimates and may be subject to change based on circumstances beyond our control.
-* Cancellations must be made in writing. Fees may apply depending on the stage of the project and incurred costs.
-* We are not liable for any indirect, incidental, or consequential damages arising from the services/products provided.
-* Both parties agree to maintain confidentiality regarding any proprietary information exchanged during the quotation process.
-* These terms shall be governed by the laws of Islamic Republic of Pakistan.</textarea>
-											</div>
+										
 									  </div>
 									</div>
 						
@@ -178,32 +160,7 @@
 													<input type="text" id="total_quantity" placeholder="Total Quantity" class="form-control" disabled>
 												</div>
 
-												<!-- <div class="col-sm-2 col-md-2 pb-sm-3 pb-md-0">
-													<label class="col-form-label">GST</label>
-													<input type="text" id="gst" name="gst_pur" onchange="netTotal()" placeholder="GST" class="form-control">
-												</div> -->
-
-												<div class="col-6 col-md-2 pb-sm-3 pb-md-0">
-													<label class="col-form-label">Convance Charges</label>
-													<input type="text" id="convance_charges" onchange="netTotal()" name="ConvanceCharges" placeholder="Convance Charges" class="form-control">
-												</div>
-
-												<div class="col-6 col-md-2 pb-sm-3 pb-md-0">
-													<label class="col-form-label">Labour Charges</label>
-													<input type="text" id="labour_charges"  onchange="netTotal()" name="LaborCharges" placeholder="Labour Charges" class="form-control">
-												</div>
-
-												<div class="col-12 col-md-2 pb-sm-3 pb-md-0">
-													<label class="col-form-label">Bill Discount </label>
-													<div class="row">
-														<div class="col-8">
-															<input type="number" id="bill_discount" onchange="netTotal()" name="Bill_discount" placeholder="Bill Discount" class="form-control">
-														</div>
-														<div class="col-4">
-															<input type="number"  id="bill_perc" class="form-control" placeholder="0%" disabled>
-														</div>
-													</div>
-												</div>
+												
 
 												<div class="col-12 pb-sm-3 pb-md-0 text-end">
 													<h3 class="font-weight-bold mt-3 mb-0 text-5 text-primary">Net Amount</h3>
@@ -217,8 +174,8 @@
 									<footer class="card-footer">
 										<div class="row form-group mb-2">
 											<div class="text-end">
-												<button type="button" class="btn btn-danger mt-2"  onclick="window.location='{{ route('all-tquotation') }}'"> <i class="fas fa-trash"></i> Discard Invoice</button>
-												<button type="submit" class="btn btn-primary mt-2"> <i class="fas fa-save"></i> Add Quotation</button>
+												<button type="button" class="btn btn-danger mt-2"  onclick="window.location='{{ route('all-weight') }}'"> <i class="fas fa-trash"></i> Discard Invoice</button>
+												<button type="submit" class="btn btn-primary mt-2"> <i class="fas fa-save"></i> Save</button>
 											</div>
 										</div>
 									</footer>
@@ -368,7 +325,7 @@
 			function getavailablestock(item_id,row_no){				
 				$.ajax({
 					type: "GET",
-					url: "/tquotation/getavailablestock/"+item_id,
+					url: "/weight/getavailablestock/"+item_id,
 					success: function(result){
 						$('#stock'+row_no).val(result[0]['opp_bal']);
 					},
