80a3be660afc1d648ee51f8f1a5957e96ddd02e2
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-10-12 22:04:33 +0500

    Update JV2Controller.php

--- app/Http/Controllers/JV2Controller.php
+++ app/Http/Controllers/JV2Controller.php
@@ -164,11 +164,12 @@ class JV2Controller extends Controller
         $jv2_items = lager::where('lager.auto_lager',$id)->get();
         $acc = AC::where('status', 1)->orderBy('ac_name', 'asc')->get();
 
-        $sales_ageing = sales_ageing::where('sales_ageing.jv2_id', $id)
+        $sales_ageing = SalesAgeing::where('sales_ageing.jv2_id', $id)
         ->join('vw_union_sale_1_2_opbal', function($join) {
             $join->on('vw_union_sale_1_2_opbal.prefix', '=', 'sales_ageing.sales_prefix')
                  ->where('vw_union_sale_1_2_opbal.Sal_inv_no', '=', 'sales_ageing.sales_id');
         })
+        ->select('sales_ageing.*', 'vw_union_sale_1_2_opbal.*')
         ->get();
         die(print_r($sales_ageing));
         $purchase_ageing = purchase_ageing::where('purchase_ageing.jv2_id',$id)->get();
