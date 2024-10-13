36f5feaf0f43e29d7b53159d1a2029eb291efa97
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-10-13 16:04:47 +0500

    Update jv2-edit.blade.php

--- resources/views/vouchers/jv2-edit.blade.php
+++ resources/views/vouchers/jv2-edit.blade.php
@@ -125,7 +125,7 @@
 										<div class="card-body">
 											<div class="row form-group mb-2">
 												<div class="col-4 mb-2">
-													<label class="col-form-label">Account Name <a><i class="fa-solid fa-arrows-rotate"></i></a></label>
+													<label class="col-form-label">Account Name <a><i class="fa fa-arrows-rotate"></i></a></label>
 													<select data-plugin-selecttwo class="form-control select2-js" id="customer_name"   onchange="getPendingInvoices()" required disabled>
 														<option value="0" selected>Select Account</option>
 														@foreach($acc as $key1 => $row1)	
