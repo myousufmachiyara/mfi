07b760ecd373e8c45870b9852be2d9a41ca67e3e
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-10-16 16:32:04 +0500

    Update jv2-new.blade.php

--- resources/views/vouchers/jv2-new.blade.php
+++ resources/views/vouchers/jv2-new.blade.php
@@ -501,7 +501,7 @@
 							html+= "<td width='15%'>"+v['sa_date']+"<input type='hidden' class='form-control' value="+v['sa_date']+"></td>"					
 							html+= "<td width='20%'><input type='number' class='form-control' value="+Math.round(v['b_amt'])+" disabled><input type='hidden' name='balance_amount[]' class='form-control' value="+Math.round(v['b_amt'])+"></td>"
 							html+= "<td width='20%'><input type='number' class='form-control text-danger'  value="+Math.round(v['balance'])+" disabled><input type='hidden' name='bill_amount[]' class='form-control' value="+Math.round(v['bill_balance'])+"></td>"
-							html+= "<td width='20%'><input type='number' class='form-control' value='0' max="+Math.round(v['balance'])+" step='any' name='rec_amount[]' onchange='totalReci()' required></td>"
+							html+= "<td width='20%'><input type='number' class='form-control' value='0' max="+Math.round(v['balance'])+" step='any' name='rec_amount[]' onchange='totalPay()' required></td>"
 							html+="</tr>";
 							$('#purpendingInvoices').append(html);
 							counter++;
