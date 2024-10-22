4f3ce06516e4532135aeabfcd18712bea7ac99b8
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-10-22 11:07:21 +0500

    Update homepageheader.blade.php

--- resources/views/layouts/homepageheader.blade.php
+++ resources/views/layouts/homepageheader.blade.php
@@ -71,13 +71,3 @@
 		</form>
 	</div>
 </header>
-<script>
-	$(document).ready(function() {
-		$(window).keydown(function(event){
-			if(event.keyCode == 13) {
-				event.preventDefault();
-				return false;
-			}
-		});
-	});
-</script>
