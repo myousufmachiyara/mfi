58ae776564b970675a161f0c405f8a1537899be3
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-10-26 17:54:24 +0500

    Update ReportingController.php

--- app/Http/Controllers/ReportingController.php
+++ app/Http/Controllers/ReportingController.php
@@ -32,6 +32,7 @@ class ReportingController extends Controller
             ->whereBetween('DATE', [$request->fromDate, $request->toDate])
             ->get();
 
+        return $pur_by_account;
         return Excel::download(new Purchase1Export($pur_by_account), 'custom_data.xlsx');
     }
 
