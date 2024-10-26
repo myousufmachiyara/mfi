6a3ba053aa9f43ee68b81ee118dd3c8387a2aa1e
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-10-26 17:44:33 +0500

    fixes

---
+++ app/Exports/Purchase1Export.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\pur_by_account;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class Purchase1Export implements FromCollection, WithHeadings
+{
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function collection()
+    {
+        return $this->data;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Column 1',
+            'Column 2',
+            // Add your actual column names here
+        ];
+    }
+}
--- app/Http/Controllers/ReportingController.php
+++ app/Http/Controllers/ReportingController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\AC;
 use App\Models\pur_by_account;
 use App\Models\pipe_pur_by_account;
-use App\Exports\CustomExport;
+use App\Exports\Purchase1Export;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ReportingController extends Controller
@@ -26,14 +26,15 @@ class ReportingController extends Controller
         return $pur_by_account;
     }
 
-    public function purchase1Excel(Request $request){
-        $pur_by_account = pur_by_account::where('ac1',$request->acc_id)
-        ->whereBetween('DATE', [$request->fromDate, $request->toDate])
-        ->get();
+    public function purchase1Excel(Request $request)
+    {
+        $pur_by_account = PurByAccount::where('ac1', $request->acc_id)
+            ->whereBetween('DATE', [$request->fromDate, $request->toDate])
+            ->get();
 
-        return Excel::download(new CustomExport($pur_by_account), 'custom_data.xlsx');
+        return Excel::download(new Purchase1Export($pur_by_account), 'custom_data.xlsx');
     }
-    
+
     public function purchase2(Request $request){
         $pipe_pur_by_account = pipe_pur_by_account::where('ac1',$request->acc_id)
         ->whereBetween('date', [$request->fromDate, $request->toDate])
