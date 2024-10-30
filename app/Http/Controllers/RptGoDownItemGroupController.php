fee2a964d954080842f61c0a649b8befb6c7e84e
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-10-31 01:13:21 +0500

    Update RptGoDownItemGroupController.php

--- app/Http/Controllers/RptGoDownItemGroupController.php
+++ app/Http/Controllers/RptGoDownItemGroupController.php
@@ -19,25 +19,23 @@ class RptGoDownItemGroupController extends Controller
         $itemStockData = ItemGroup::with(['itemEntries.pipeItemStock'])
             ->where('item_group_cod', $request->acc_id)
             ->get()
-            ->map(function ($itemGroup) {
-                return $itemGroup->itemEntries->map(function ($itemEntry) {
-                    // Filter out items with zero opening balance
-                    if ($itemEntry->pipeItemStock->opp_bal != 0) {
-                        return [
-                            'item_group_cod' => $itemGroup->item_group_cod,
-                            'it_cod' => $itemEntry->it_cod,
-                            'opp_bal' => $itemEntry->pipeItemStock->opp_bal,
-                            'wt' => $itemEntry->pipeItemStock->opp_bal * $itemEntry->weight,
-                            'item_name' => $itemEntry->item_name,
-                            'item_remark' => $itemEntry->item_remark,
-                        ];
-                    }
-                })->filter(); // Remove null values
-            })->flatten(1);
+            ->flatMap(function ($itemGroup) {
+                return $itemGroup->itemEntries->filter(function ($itemEntry) {
+                    return $itemEntry->pipeItemStock->opp_bal != 0;
+                })->map(function ($itemEntry) use ($itemGroup) {
+                    return [
+                        'item_group_cod' => $itemGroup->item_group_cod,
+                        'it_cod' => $itemEntry->it_cod,
+                        'opp_bal' => $itemEntry->pipeItemStock->opp_bal,
+                        'wt' => $itemEntry->pipeItemStock->opp_bal * $itemEntry->weight,
+                        'item_name' => $itemEntry->item_name,
+                        'item_remark' => $itemEntry->item_remark,
+                    ];
+                });
+            });
     
         return response()->json($itemStockData);
     }
     
     
-
 }
