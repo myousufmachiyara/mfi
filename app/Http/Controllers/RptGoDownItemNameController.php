289d09b57ca4752f0ded5192c3d087707644d77e
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-10-30 21:25:36 +0500

    Update RptGoDownItemNameController.php

--- app/Http/Controllers/RptGoDownItemNameController.php
+++ app/Http/Controllers/RptGoDownItemNameController.php
@@ -256,12 +256,12 @@ class RptGoDownItemNameController extends Controller
                 <td style="font-size:12px; font-weight:bold; color:#17365D; border-bottom:1px solid #000; width:70%;">
                     Item Name: <span style="color:black;">' . $gd_pipe_sale_by_item_name[0]['item_name'] . '</span>
                 </td>
-                <td style="font-size:12px; color:#17365D; text-align:left; border-bottom:1px solid #000;border-left:1px solid #000; width:30%;">
+                <td style="font-size:12px; font-weight:bold;color:#17365D; text-align:left; border-bottom:1px solid #000;border-left:1px solid #000; width:30%;">
                     Print Date: <span style="color:black;">' . $formattedDate . '</span>
                 </td>
             </tr>
             <tr>
-                <td style="font-size:12px; font-weight:bold; color:#17365D; border-bottom:1px solid #000;width:70%;">
+                <td style="font-size:12px; color:#17365D; border-bottom:1px solid #000;width:70%;">
                     Item Remarks: <span style="color:black;">' . $gd_pipe_sale_by_item_name[0]['item_remark'] . '</span>
                 </td>
                 <td style="font-size:12px; font-weight:bold; color:#17365D; text-align:left; border-bottom:1px solid #000;border-left:1px solid #000; width:30%;">
