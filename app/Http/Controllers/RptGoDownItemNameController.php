785f2b96178b9a5e29761a08899041f4cb375105
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-10-30 15:50:32 +0500

    Update RptGoDownItemNameController.php

--- app/Http/Controllers/RptGoDownItemNameController.php
+++ app/Http/Controllers/RptGoDownItemNameController.php
@@ -99,7 +99,7 @@ class RptGoDownItemNameController extends Controller
     
         // Header details
         $html = '
-        <table style="border:1px solid #000; width:100%; padding:10px;">
+        <table style="border:1px solid #000; width:100%; padding:8px;">
             <tr>
                 <td style="font-size:12px; font-weight:bold; color:#17365D; border-bottom:1px solid #000;">
                     Item Name: <span style="color:black;">' . $gd_pipe_pur_by_item_name[0]['item_name'] . '</span>
