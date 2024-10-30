153e86af67b43a54b07d38eb3f539a30bc52b52b
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-10-30 16:02:36 +0500

    Update RptGoDownItemNameController.php

--- app/Http/Controllers/RptGoDownItemNameController.php
+++ app/Http/Controllers/RptGoDownItemNameController.php
@@ -104,7 +104,7 @@ class RptGoDownItemNameController extends Controller
                 <td style="font-size:12px; font-weight:bold; color:#17365D; border-bottom:1px solid #000; width:70%;">
                     Item Name: <span style="color:black;">' . $gd_pipe_pur_by_item_name[0]['item_name'] . '</span>
                 </td>
-                <td style="font-size:12px; font-weight:bold; color:#17365D; text-align:right; border-bottom:1px solid #000;border-left:1px solid #000; width:35%;">
+                <td style="font-size:12px; font-weight:bold; color:#17365D; text-align:right; border-bottom:1px solid #000;border-left:1px solid #000; width:20%;">
                     Print Date: <span style="color:black;">' . $formattedDate . '</span>
                 </td>
             </tr>
