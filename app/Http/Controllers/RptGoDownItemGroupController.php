139c6228373c1de1c5e40926e86f22147c0230a4
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-10-31 01:03:49 +0500

    Update RptGoDownItemGroupController.php

--- app/Http/Controllers/RptGoDownItemGroupController.php
+++ app/Http/Controllers/RptGoDownItemGroupController.php
@@ -16,11 +16,12 @@ class RptGoDownItemGroupController extends Controller
 {
     public function tstockall(Request $request)
     {
-        $gd_pipe_item_stock9_much = gd_pipe_item_stock9_much::where('gd_pipe_item_stock9_much.it_cod', $request->acc_id)
-            ->join('item_entry2', 'gd_pipe_item_stock9_much.it_cod', '=', 'item_entry2.it_cod')
-            ->join('item_group', 'item_entry2.item_group', '=', 'item_group.item_group_cod')
+
+        $gd_pipe_item_stock9_much = Item_Groups::where('item_groups.item_group_cod', $request->acc_id)
+            ->join('item_entry2', 'item_groups.item_group_cod', '=', 'item_entry2.item_group')
+            ->join('gd_pipe_item_stock9_much', 'item_entry2.it_cod', '=', 'gd_pipe_item_stock9_much.it_cod')
             ->select(
-                'item_group.item_group_cod',
+                'item_groups.item_group_cod',
                 'gd_pipe_item_stock9_much.it_cod',
                 'gd_pipe_item_stock9_much.opp_bal',
                 DB::raw('gd_pipe_item_stock9_much.opp_bal * item_entry2.weight AS wt'),
@@ -29,9 +30,9 @@ class RptGoDownItemGroupController extends Controller
             )
             ->where('gd_pipe_item_stock9_much.opp_bal', '<>', 0)
             ->get();
-    
-        return $gd_pipe_item_stock9_much;
-    }
+
+         return response()->json($gd_pipe_item_stock9_much);
+     }
     
 
 }
