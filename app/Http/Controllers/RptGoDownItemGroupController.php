ce03a330d38953a2cd5fe18a50e13aedbdfdc221
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-10-31 01:16:52 +0500

    s

--- app/Http/Controllers/RptGoDownItemGroupController.php
+++ app/Http/Controllers/RptGoDownItemGroupController.php
@@ -14,28 +14,6 @@ use Illuminate\Validation\Validator;
 
 class RptGoDownItemGroupController extends Controller
 {
-    public function tstockall(Request $request)
-    {
-        $itemStockData = ItemGroup::with(['itemEntries.pipeItemStock'])
-            ->where('item_group_cod', $request->acc_id)
-            ->get()
-            ->flatMap(function ($itemGroup) {
-                return $itemGroup->itemEntries->filter(function ($itemEntry) {
-                    return $itemEntry->pipeItemStock->opp_bal != 0;
-                })->map(function ($itemEntry) use ($itemGroup) {
-                    return [
-                        'item_group_cod' => $itemGroup->item_group_cod,
-                        'it_cod' => $itemEntry->it_cod,
-                        'opp_bal' => $itemEntry->pipeItemStock->opp_bal,
-                        'wt' => $itemEntry->pipeItemStock->opp_bal * $itemEntry->weight,
-                        'item_name' => $itemEntry->item_name,
-                        'item_remark' => $itemEntry->item_remark,
-                    ];
-                });
-            });
-    
-        return response()->json($itemStockData);
-    }
     
     
 }
