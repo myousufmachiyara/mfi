4f0b5044e3ee70ba5c2c7773784a49032c63610b
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-10-31 15:37:42 +0500

    Update RptGoDownItemNameController.php

--- app/Http/Controllers/RptGoDownItemNameController.php
+++ app/Http/Controllers/RptGoDownItemNameController.php
@@ -32,7 +32,7 @@ class RptGoDownItemNameController extends Controller
         $gd_pipe_pur_by_item_name = gd_pipe_pur_by_item_name::where('item_cod',$request->acc_id)
         ->leftjoin('ac','gd_pipe_pur_by_item_name.ac_cod','=','ac.ac_code')
         ->whereBetween('pur_date', [$request->fromDate, $request->toDate])
-        ->get();
+        ->select('gd_pipe_pur_by_item_name.*','ac.ac_name');
 
         $accId = $request->acc_id;
         $fromDate = \Carbon\Carbon::parse($request->fromDate)->format('Y-m-d');
