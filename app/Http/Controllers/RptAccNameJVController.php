bafc14dc3c597d051ac354155390a86fa8c0b9e0
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-10-31 08:10:39 +0500

    Update RptAccNameJVController.php

--- app/Http/Controllers/RptAccNameJVController.php
+++ app/Http/Controllers/RptAccNameJVController.php
@@ -101,11 +101,11 @@ class RptAccNameJVController extends Controller
 
         $html = '<table border="1" style="border-collapse: collapse;text-align:center" >';
         $html .= '<tr>';
-        $html .= '<th style="width:6%;color:#17365D;font-weight:bold;">S/No</th>';
-        $html .= '<th style="width:8%;color:#17365D;font-weight:bold;">Voucher</th>';
-        $html .= '<th style="width:12%;color:#17365D;font-weight:bold;">Date</th>';
-        $html .= '<th style="width:22%;color:#17365D;font-weight:bold;">Account Name</th>';
-        $html .= '<th style="width:25%;color:#17365D;font-weight:bold;">Remarks</th>';
+        $html .= '<th style="width:7%;color:#17365D;font-weight:bold;">S/No</th>';
+        $html .= '<th style="width:8%;color:#17365D;font-weight:bold;">Vouc</th>';
+        $html .= '<th style="width:10%;color:#17365D;font-weight:bold;">Date</th>';
+        $html .= '<th style="width:23%;color:#17365D;font-weight:bold;">Account Name</th>';
+        $html .= '<th style="width:27%;color:#17365D;font-weight:bold;">Remarks</th>';
         $html .= '<th style="width:12%;color:#17365D;font-weight:bold;">Debit</th>';
         $html .= '<th style="width:12%;color:#17365D;font-weight:bold;">Credit</th>';
         $html .= '</tr>';
@@ -124,7 +124,7 @@ class RptAccNameJVController extends Controller
                 $html .= '<tr style="background-color:#f1f1f1">';
                 $html .= '<td style="width:6%;">'.$count.'</td>';
                 $html .= '<td style="width:8%;">'.$items['entry_of'].'</td>';
-                $html .= '<td style="width:12%;">'.Carbon::createFromFormat('Y-m-d', $items['jv_date'])->format('d-m-y').'</td>';
+                $html .= '<td style="width:10%;">'.Carbon::createFromFormat('Y-m-d', $items['jv_date'])->format('d-m-y').'</td>';
                 $html .= '<td style="width:22%;">'.$items['ac2'].'</td>';
                 $html .= '<td style="width:25%;">'.$items['Narration'].'</td>';
                 $html .= '<td style="width:12%;">'.$items['Debit'].'</td>';
@@ -137,7 +137,7 @@ class RptAccNameJVController extends Controller
                 $html .= '<tr>';
                 $html .= '<td style="width:6%;">'.$count.'</td>';
                 $html .= '<td style="width:8%;">'.$items['entry_of'].'</td>';
-                $html .= '<td style="width:12%;">'.Carbon::createFromFormat('Y-m-d', $items['jv_date'])->format('d-m-y').'</td>';
+                $html .= '<td style="width:10%;">'.Carbon::createFromFormat('Y-m-d', $items['jv_date'])->format('d-m-y').'</td>';
                 $html .= '<td style="width:22%;">'.$items['ac2'].'</td>';
                 $html .= '<td style="width:25%;">'.$items['Narration'].'</td>';
                 $html .= '<td style="width:12%;">'.$items['Debit'].'</td>';
