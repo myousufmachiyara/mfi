dd10b8798982ea93181941acee301826e93497f2
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-11-01 19:37:50 +0500

    Update RptDailyRegPur1Controller.php

--- app/Http/Controllers/RptDailyRegPur1Controller.php
+++ app/Http/Controllers/RptDailyRegPur1Controller.php
@@ -24,7 +24,7 @@ class RptDailyRegPur1Controller extends Controller
     public function pur1Excel(Request $request)
     {
         $activite7_pur = activite7_pur::whereBetween('pur_date', [$request->fromDate, $request->toDate])
-        ->join('ac','ac.ac_code','=','activite7_pur.account_name')
+        ->join('ac','ac.ac_code','=','activite7_pur.ac_cod')
         ->select('activite7_pur.*','ac.ac_name as acc_name') 
         ->get();
 
