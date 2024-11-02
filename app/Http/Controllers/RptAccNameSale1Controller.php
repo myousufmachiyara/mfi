e974969a55d30126c86f4fc6d23fdf08df6a34ae
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-02 17:24:48 +0500

    Update RptAccNameSale1Controller.php

--- app/Http/Controllers/RptAccNameSale1Controller.php
+++ app/Http/Controllers/RptAccNameSale1Controller.php
@@ -55,8 +55,8 @@ class RptAccNameSale1Controller extends Controller
         $pdf = new MyPDF();
         $pdf->SetCreator(PDF_CREATOR);
         $pdf->SetAuthor('MFI');
-        $pdf->SetTitle("Sale Report Of Account {$request->acc_id}");
-        $pdf->SetSubject("Sale Report Of Account {$request->acc_id}");
+        $pdf->SetTitle("Sale Report Of Account {$request->ac_name}");
+        $pdf->SetSubject("Sale Report Of Account {$request->ac_name}");
         $pdf->SetKeywords('Sale Report, TCPDF, PDF');
         $pdf->setPageOrientation('P');
         $pdf->AddPage();
