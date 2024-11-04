536949bc6eec44cbe68b7c4cb112861e7352adaf
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-04 12:50:52 +0500

    Update gd_item_name.blade.php

--- resources/views/reports/gd_item_name.blade.php
+++ resources/views/reports/gd_item_name.blade.php
@@ -323,13 +323,16 @@
                         // $(tableID).append(html);
 
                         var html = "<tr>";
-                        html += "<th colspan=5></th>"; // Leave columns up to "Remarks" empty
-                        html += "<th style='text-align:right'>Opening Quantity:</th>"; // Place label in "Remarks" column
-                        html += "<th></th>"; // Empty cell after "Remarks"
-                        html += "<th></th>"; // Empty cell after "Remarks"
-                        html += "<th colspan=2>" + opening_qty + "</th>"; // Display opening quantity across the last two columns
-                        html += "</tr>";
-                        $(tableID).append(html);
+                            html += "<th></th>"; // Empty cell after "Remarks"
+                            html += "<th></th>"; // Empty cell after "Remarks"
+                            html += "<th></th>"; // Empty cell after "Remarks"
+                            html += "<th></th>"; // Empty cell after "Remarks"
+                            html += "<th style='text-align:right'>Opening Quantity:</th>"; // Place label in "Remarks" column
+                            html += "<th></th>"; // Empty cell after "Remarks"
+                            html += "<th></th>"; // Empty cell after "Remarks"
+                            html += "<th colspan=2>" + opening_qty + "</th>"; // Display opening quantity across the last two columns
+                            html += "</tr>";
+                            $(tableID).append(html);
 
 
 
