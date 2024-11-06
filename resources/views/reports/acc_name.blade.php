c93420953cadeda9076af99efe68592315b32aeb
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-06 17:28:16 +0500

    Update acc_name.blade.php

--- resources/views/reports/acc_name.blade.php
+++ resources/views/reports/acc_name.blade.php
@@ -153,7 +153,7 @@
                                             
                                             <h4 class="mb-0 h6 mb-1 text-dark font-weight-semibold">
                                                 <span style="color:#17365D">Account Name: &nbsp;</span>
-                                                <span style="font-weight:400; color:black;font-size:20px;" id="sale_2_acc" class="text-danger"></span>
+                                                <span style="font-weight:400; color:rgb(238, 19, 19);font-size:20px;" id="sale_2_acc"></span>
                                             </h4>
                                         </div>
                                     </div>
