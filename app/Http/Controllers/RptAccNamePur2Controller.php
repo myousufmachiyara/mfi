92c023b56d3430bef781ff4d13251b025252f776
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-06 18:55:08 +0500

    Update RptAccNamePur2Controller.php

--- app/Http/Controllers/RptAccNamePur2Controller.php
+++ app/Http/Controllers/RptAccNamePur2Controller.php
@@ -15,6 +15,7 @@ class RptAccNamePur2Controller extends Controller
     public function purchase2(Request $request){
         $pipe_pur_by_account = pipe_pur_by_account::where('ac1',$request->acc_id)
         ->whereBetween('date', [$request->fromDate, $request->toDate])
+        ->orderBy('date', 'asc')
         ->get();
 
         return $pipe_pur_by_account;
