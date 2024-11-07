32422c4477ec38cd02a607b4c5469910c64d3d50
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-11-07 17:09:09 +0500

    Update gd_group_name.blade.php

--- resources/views/reports/gd_group_name.blade.php
+++ resources/views/reports/gd_group_name.blade.php
@@ -488,8 +488,6 @@
                             var html="<tr>";
                             html += "<td>"+ (k ? k : "") +"</td>";
 
-
-                            
                             let table1 = document.getElementById('TSAThead');
 
                             // Get the first row (<tr>) inside the <thead> section
@@ -501,59 +499,59 @@
                             for(i=1;i<columnCount;i++){
                                 console.log(i);
                             }
-                            // $.each(v, function(l, m) {
-                            //     if(m['item_mm']=="12G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="14G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="16G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="1.5"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="18G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="1.10"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="19G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="20G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="21G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="22G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else if(m['item_mm']=="23G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     if(m['item_mm']=="24G"){
-                            //         // set value in 1st coloum
-                            //         html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
-                            //     }
-                            //     else{
-                            //         html += "<td>-</td>"
-                            //     }
-                            // });
+                            $.each(v, function(l, m) {
+                                if(m['item_mm']=="12G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="14G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="16G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="1.5"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="18G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="1.10"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="19G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="20G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="21G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="22G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else if(m['item_mm']=="23G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                if(m['item_mm']=="24G"){
+                                    // set value in 1st coloum
+                                    html += "<td>"+ (m['opp_bal'] ? m['opp_bal'] : "") +"</td>"
+                                }
+                                else{
+                                    html += "<td>-</td>"
+                                }
+                            });
                             html +="</tr>";
                             $(tableID).append(html);
                         });
