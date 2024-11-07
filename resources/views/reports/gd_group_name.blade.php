6520fd51ac0eac1f6e8263111f5cdee03af7f260
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-07 18:23:33 +0500

    Update gd_group_name.blade.php

--- resources/views/reports/gd_group_name.blade.php
+++ resources/views/reports/gd_group_name.blade.php
@@ -497,11 +497,11 @@
                             let columnCount = firstRow ? firstRow.cells.length : 0;
                             console.log("col tot:"+columnCount)
 
-                            const header = table1.querySelector("thead tr");
+                            const header = table1.querySelector("thead tr th");
 
                             for(i=0;i<columnCount-1;i++){
-                                const th = header.cells[columnCount+1]; // Get the cell at the specified index
-                                var col_id = th ? th.id : null; // Return the ID, or null if not found
+                                const th = header[columnCount+1]; // Get the cell at the specified index
+                                let col_id = th ? th.id : null; // Return the ID, or null if not found
                                 console.log(col_id);
 
                                 if(i<=v.length){
