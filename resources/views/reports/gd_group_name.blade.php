cc99ef2d61542268fbbf2b203cec69617b8453f8
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-07 18:04:07 +0500

    Update gd_group_name.blade.php

--- resources/views/reports/gd_group_name.blade.php
+++ resources/views/reports/gd_group_name.blade.php
@@ -503,7 +503,7 @@
                                 const th = header.cells[columnCount+1]; // Get the cell at the specified index
                                 var col_id = th ? th.id : null; // Return the ID, or null if not found
 
-                                if(i<v.length && ){
+                                if(i<v.length){
                                     if(col_id == v[i]['item_mm']){
                                         html += "<td>"+ (v[i]['opp_bal'] ? v[i]['opp_bal'] : "0") +"</td>"
                                     }
