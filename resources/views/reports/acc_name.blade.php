48e036a7122b00bc3271e933548c9789b6d87bce
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-09 15:59:45 +0500

    Update acc_name.blade.php

--- resources/views/reports/acc_name.blade.php
+++ resources/views/reports/acc_name.blade.php
@@ -580,7 +580,7 @@
                             // After the loop, add the totals row
                             var netAmount = balance; 
                             var words = convertCurrencyToWords(netAmount);
-                            var totalHtml = "<tr><td class='text-danger' colspan='5'><strong>" + words + "</strong></td>";
+                            var totalHtml = "<tr><td style='color:#17365D' colspan='5'><strong>" + words + "</strong></td>";
                             totalHtml += "<td style='text-align: right;'><strong>Total</strong></td>";
                             totalHtml += "<td class='text-danger'><strong>" + totalDebit.toFixed(0) + "</strong></td>";
                             totalHtml += "<td class='text-danger'><strong>" + totalCredit.toFixed(0) + "</strong></td>";
