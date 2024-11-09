ee499368b4dd7cff670dcf6bb3c9c7ab3e608028
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-11-09 15:03:42 +0500

    Update RptAccGrpBAController.php

--- app/Http/Controllers/RptAccGrpBAController.php
+++ app/Http/Controllers/RptAccGrpBAController.php
@@ -12,8 +12,7 @@ use Carbon\Carbon;
 class RptAccGrpBAController extends Controller
 {
     public function ba(Request $request){
-        $balance_all = balance_all::where('ac_code',$request->acc_id)
-        ->groupby('heads')
+        $balance_all = balance_all::groupby('heads')
         ->get();
 
         return $balance_all;
