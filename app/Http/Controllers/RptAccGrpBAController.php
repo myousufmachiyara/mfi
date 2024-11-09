196fc34770233c07b0bd59e319e5faf57947309a
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-11-09 18:35:58 +0500

    Update RptAccGrpBAController.php

--- app/Http/Controllers/RptAccGrpBAController.php
+++ app/Http/Controllers/RptAccGrpBAController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\balance_all;
-use App\Models\AC;
 use App\Exports\PurchaseCombExport;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Services\myPDF;
@@ -14,9 +13,8 @@ use Illuminate\Support\Facades\DB;
 class RptAccGrpBAController extends Controller
 {
     public function ba(Request $request){
-        $balance_all = balance_all::leftjoin('ac', 'ac.ac_code', '=', 'balance_all.ac_code')
-        ->groupBy('balance_all.heads', 'ac.ac_name', 'ac.address')  // Include all non-aggregated columns in groupBy
-        ->select('balance_all.heads', 'ac.ac_name', 'ac.address')  // Select these columns
-        ->get();
+        $balance_all = balance_all::all()->groupBy('heads');
+
+        return $balance_all;
     }
 }
