3fb273653b4984947008be4fdb8f5066f88a64d0
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-09 17:42:21 +0500

    Update acc_name.blade.php

--- resources/views/reports/acc_name.blade.php
+++ resources/views/reports/acc_name.blade.php
@@ -844,7 +844,7 @@
                         });
 
                         // Display the total in the last row or specific cell
-                        var totalRow = "<tr><td colspan='6' style='text-align: right;'><strong>Total:</strong></td>";
+                        var totalRow = "<tr><td colspan='7' style='text-align: right;'><strong>Total:</strong></td>";
                         totalRow += "<td class='text-danger'><strong>" + totalCrAmt.toFixed(0) + "</strong></td></tr>";
                         $(tableID).append(totalRow);
                     },
