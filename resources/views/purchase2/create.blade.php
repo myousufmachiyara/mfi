7bc4917f7b404eb3531b6b9cd4ca8cf3d547a6ab
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-10 16:29:28 +0500

    Update create.blade.php

--- resources/views/purchase2/create.blade.php
+++ resources/views/purchase2/create.blade.php
@@ -101,7 +101,7 @@
 										<div class="row form-group mb-2">
 											<div class="col-6 mb-2">
 												<label class="col-form-label" >Basic Amount<span style="color: red;"><strong>*</strong></span></label>
-												<input type="number" name="bamount" onchange="CalBillAfterDisc()" autofocus id="basic_amount" required value="0" step="any" class="form-control comm-form-field" disabled>
+												<input type="number" name="bamount" onchange="CalBillAfterDisc()"  autofocus id="basic_amount" required value="0" step="any" class="form-control comm-form-field" disabled>
 											</div>
 											<div class="col-2 mb-2">
 												<label class="col-form-label" >%<span style="color: red;"><strong>*</strong></span></label>
@@ -128,13 +128,21 @@
 												<input type="number" value="0" name="comm_amount" step="any" required class="form-control comm-form-field" disabled>
 											</div>
 
-											<div class="col-6 mb-2">
+											<div class="col-3 mb-2">
 												<label class="col-form-label" >GST<span style="color: red;"><strong>*</strong></span></label>
-												<input type="number" value="0" name="gst" step="any" required class="form-control comm-form-field" disabled>
+												<input type="number" value="18" name="gst" id="gst_id" onchange="CalGSTAmount()" step="any" required class="form-control comm-form-field" disabled>
 											</div>
-											<div class="col-6 mb-2">
+											<div class="col-3 mb-2">
+												<label class="col-form-label" >GST Amount</label>
+												<input type="number" value="0" id="GSTAmount"  step="any" required disabled class="form-control comm-form-field">
+											</div>
+											<div class="col-3 mb-2">
 												<label class="col-form-label" >Income Tax<span style="color: red;"><strong>*</strong></span></label>
-												<input type="number" value="0" name="income_tax" step="any" required class="form-control comm-form-field" disabled>
+												<input type="number" value="0" name="income_tax" id="it_id" step="any" onchange="CalITAmount()" required class="form-control comm-form-field" disabled>
+											</div>
+											<div class="col-3 mb-2">
+												<label class="col-form-label" >IT Amount</label>
+												<input type="number" value="0" id="ITAmount"  step="any" required disabled class="form-control comm-form-field">
 											</div>
 
 											<div class="col-sm-12 col-md-6 mb-2">
@@ -421,8 +429,36 @@
 		var basic_amount_disc = parseFloat($('#basic_amount_disc').val());
 
 		sum= ((basic_amount * basic_amount_disc )/100)+basic_amount;
-		$('#BillAfterDisc').val(sum);
+		$('#BillAfterDisc').val(sum.toFixed(0));
+
+		 // Update GST and IT amount based on the discounted bill
+		 CalGSTAmount();
+		 CalITAmount();
 	}
+
+	function CalGSTAmount() {
+        var gst_id = parseFloat($('#gst_id').val());
+        var BillAfterDisc = parseFloat($('#BillAfterDisc').val());
+
+        if (!isNaN(gst_id) && !isNaN(BillAfterDisc)) {
+            var sum = (BillAfterDisc * gst_id) / 100;
+            $('#GSTAmount').val(sum.toFixed(0));
+        }
+    }
+
+	function CalITAmount() {
+		var it_id = parseFloat($('#it_id').val());
+		var GSTAmount = parseFloat($('#GSTAmount').val());
+		var BillAfterDisc = parseFloat($('#BillAfterDisc').val());
+
+		if (!isNaN(it_id) && !isNaN(BillAfterDisc) && !isNaN(GSTAmount)) {
+			// Calculate the income tax amount
+			var sum = ((BillAfterDisc + GSTAmount) * it_id) / 100;
+			$('#ITAmount').val(sum.toFixed(0));
+		}
+	}
+
+
 
 	function getCOADetails(){
 		var coaId = document.getElementById("coa_name").value;
