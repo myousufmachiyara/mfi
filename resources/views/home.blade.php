01afbbb370cae2facc012c220f5afbd1fca1a9d1
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-11-12 10:31:04 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -287,5 +287,25 @@
 				}
 			}
 		}); 
+
+
+		const top5Customers = document.getElementById('top5Customers');
+
+		new Chart(top5Customers, {
+			type: 'doughnut',
+			data: {
+				labels: ['Red','Blue','Yellow'],
+				datasets: [{
+					label: 'My First Dataset',
+					data: [300, 50, 100],
+					backgroundColor: [
+						'rgb(255, 99, 132)',
+						'rgb(54, 162, 235)',
+						'rgb(255, 205, 86)'
+					],
+					hoverOffset: 4
+				}]
+			},
+		});
 	</script>
 </html>
