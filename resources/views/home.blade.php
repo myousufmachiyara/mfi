9a12f2ecbd47e1fdc9123c1d0bb4e7348e36678c
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2024-11-12 10:51:26 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -306,18 +306,19 @@
 		
 		const halfYearSale2 = document.getElementById('halfYearSale2');
 		const NUMBER_CFG = {count: 6, min: -100, max: 100};
+
 		const data = {
 			labels: labels,
 			datasets: [
 				{
 				label: 'Dataset 1',
-				data: Utils.numbers(NUMBER_CFG),
+				data: Utils.months(NUMBER_CFG),
 				borderColor: Utils.CHART_COLORS.red,
 				backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
 				},
 				{
 				label: 'Dataset 2',
-				data: Utils.numbers(NUMBER_CFG),
+				data: Utils.months(NUMBER_CFG),
 				borderColor: Utils.CHART_COLORS.blue,
 				backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
 				}
