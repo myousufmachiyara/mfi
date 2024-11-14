6d34319692e644956e9c643239ab84d06847c646
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-14 16:01:14 +0500

    fixes

--- resources/views/layouts/homepageheader.blade.php
+++ resources/views/layouts/homepageheader.blade.php
@@ -4,11 +4,11 @@
     </div>
 </div>
 <header class="page-header">
+	
 	<div class="logo-container d-md-none">
 		<a href="/" class="logo ">
 			<img src="/assets/img/logo.png" width="70px" alt="MFI Logo" />
 		</a>
-
 		<div id="userbox" class="userbox" style="float:right !important;">
 			<a href="#" data-bs-toggle="dropdown" style="margin-right: 20px;">
 				<div class="profile-info"> 
@@ -28,13 +28,35 @@
 				</ul>
 			</div>
 			<i class="fas fa-bars toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened" aria-label="Toggle sidebar"></i>
-		<div class="toggle-sidebar-left" >
 		</div>
+		<div class="right-wrapper text-end d-none d-md-block">
+			<div id="userbox" class="userbox" style="float:right !important;">
+				<a href="#" data-bs-toggle="dropdown">
+					<div class="profile-info"> 
+						<span class="name">{{session('user_name')}}</span>
+						<span class="role">{{session('role_name')}}</span>
+					</div>
+					<i class="fa custom-caret"></i>
+				</a>
+				<div class="dropdown-menu" >
+					<ul class="list-unstyled">
+						<li>
+							<a role="menuitem" tabindex="-1" href="#changePassword" class="mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal"><i class="bx bx-lock"></i> Change Password</a>
+						</li>
+						<li>
+							<a role="menuitem" tabindex="-1" href="/logout"><i class="bx bx-power-off"></i> Logout</a>
+						</li>
+					</ul>
+				</div>
+				<i class="fas fa-bars toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened" aria-label="Toggle sidebar"></i>
 
+			</div>
+		</div>
 	</div>
-	<div class="right-wrapper text-end d-none d-md-block">
-		<div id="userbox" class="userbox" style="float:right !important;">
-			<a href="#" data-bs-toggle="dropdown">
+
+	<div class="logo-container d-none d-md-block">
+		<div id="userbox" class="userbox" style="float:right !important;left: -55px !important;">
+			<a href="#" data-bs-toggle="dropdown" style="margin-right: 20px;">
 				<div class="profile-info"> 
 					<span class="name">{{session('user_name')}}</span>
 					<span class="role">{{session('role_name')}}</span>
@@ -51,8 +73,6 @@
 					</li>
 				</ul>
 			</div>
-			<i class="fas fa-bars toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened" aria-label="Toggle sidebar"></i>
-
 		</div>
 	</div>
 
