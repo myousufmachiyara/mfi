46751cafef9ce41cef1eed46596b7e284630f8ba
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-11-17 17:14:04 +0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,19 +19,8 @@ class HomeController extends Controller
     public function __construct()
     {
         // $this->middleware('auth');
-        $receivables = dash_sub_head::where('sub',1)->first();
-        $payables  = dash_sub_head::where('sub',7)->first();
-        $short_term_loan = dash_sub_head::where('sub',23)->first();
-        $long_term_loan = dash_sub_head::where('sub',18)->first();
-
-        $pdc = dash_acc_group::where('group_cod',1)->first();
-        $banks = dash_acc_group::where('group_cod',2)->first();
-        $cash = dash_acc_group::where('group_cod',3)->first();
-        $foreign = dash_acc_group::where('group_cod',4)->first();
-
-        $login_users = users::where('is_login', 1)->count();
-
-        return view('home', compact('receivables','payables','short_term_loan','long_term_loan','pdc','banks','cash','foreign','login_users'));
+        return view('home');
+        
     }
 
     /**
