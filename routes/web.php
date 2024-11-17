24f2bf0ad102b838572d8f7453d63a9b85c0efbb
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-11-17 17:15:01 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -288,8 +288,9 @@
     // });
 
     Route::middleware(['auth'])->group(function () {
-        Route::get('/logout', [App\Http\Controllers\UsersController::class, 'logout']);
         Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+        Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+        Route::get('/logout', [App\Http\Controllers\UsersController::class, 'logout']);
         Route::get('/validate-user-password', [App\Http\Controllers\UsersController::class, 'getUserPassword'])->name('validate-user-password');
         Route::post('/change-user-password', [App\Http\Controllers\UsersController::class, 'updateUserPassowrd'])->name('change-user-password');
         Route::get('/item-groups/detail', [App\Http\Controllers\ItemGroupsController::class, 'getGroupDetails'])->name('get-item-group-details');
