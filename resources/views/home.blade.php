cc6b753ff3a97de3f5ea9f180f1b142f83dc40d3
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-18 17:23:20 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -98,21 +98,22 @@
 							<section class="card card-featured-left card-featured-secondary mb-2">
 								<div class="card-body">
 									<div class="widget-summary">
-										
-										<div class="row">
-											<div class="summary col-6">
-												<strong class="amount">PDC</strong>
-												<div class="info">
-													@if(isset($pdc) && isset($pdc->Total_Balance) && strpos($pdc->Total_Balance, '.') !== false && substr($pdc->Total_Balance, strpos($pdc->Total_Balance, '.') + 1) > '0')
-														<h4 class="amount m-0 text-primary"><strong class="rolling-number amount m-0 text-primary" data-target="{{ number_format($pdc->Total_Balance, 0, '.', ',') }}">0</strong><span class="title text-end text-dark"> PKR</span></h4>
-													@elseif(isset($pdc) && isset($pdc->Total_Balance))
-														<h4 class="amount m-0 text-primary"><strong class="rolling-number amount m-0 text-primary" data-target="{{ number_format($pdc->Total_Balance, 0, '.', ',') }}" >0</strong><span class="title text-end text-dark"> PKR</span></h4>
-													@else
-														<h4 class="amount m-0 text-primary"><strong>0</strong><span class="title text-end text-dark"> PKR</span></h4>
-													@endif
-												</div>
+										<div class="widget-summary-col widget-summary-col-icon">
+											<div class="summary-icon bg-secondary">
+												<i class="fas fa-university"></i> <!-- PDC Icon -->
 											</div>
-
+										</div>
+										<div class="summary col-6">
+											<strong class="amount">PDC</strong>
+											<div class="info">
+												@if(isset($pdc) && isset($pdc->Total_Balance) && strpos($pdc->Total_Balance, '.') !== false && substr($pdc->Total_Balance, strpos($pdc->Total_Balance, '.') + 1) > '0')
+													<h4 class="amount m-0 text-primary"><strong class="rolling-number amount m-0 text-primary" data-target="{{ number_format($pdc->Total_Balance, 0, '.', ',') }}">0</strong><span class="title text-end text-dark"> PKR</span></h4>
+												@elseif(isset($pdc) && isset($pdc->Total_Balance))
+													<h4 class="amount m-0 text-primary"><strong class="rolling-number amount m-0 text-primary" data-target="{{ number_format($pdc->Total_Balance, 0, '.', ',') }}" >0</strong><span class="title text-end text-dark"> PKR</span></h4>
+												@else
+													<h4 class="amount m-0 text-primary"><strong>0</strong><span class="title text-end text-dark"> PKR</span></h4>
+												@endif
+											</div>
 										</div>
 										<div>
 											<div class="summary-footer">
