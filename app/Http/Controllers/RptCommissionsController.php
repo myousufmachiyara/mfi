ef330c812b8cb5dd9c7ae0b5d6a4a8959669975a
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-18 15:09:46 +0500

    Update RptCommissionsController.php

--- app/Http/Controllers/RptCommissionsController.php
+++ app/Http/Controllers/RptCommissionsController.php
@@ -120,12 +120,16 @@ class RptCommissionsController extends Controller
             $html .= '
                 <tr style="background-color:' . $backgroundColor . ';">
                     <td style="width:7%;">' . $count . '</td>
+                    <td style="width:10%;">' . Carbon::parse($item['Date'])->format('d-m-y') . '</td>
                     <td style="width:8%;">' . $item['auto_lager']. '</td>
-                    <td style="width:11%;">' . Carbon::parse($item['Date'])->format('d-m-y') . '</td>
-                    <td style="width:18%;">' . $item['Debit_Acc'] . '</td>
-                    <td style="width:18%;">' . $item['Credit_Acc'] . '</td>
-                    <td style="width:27%;">' . $item['remarks'] . '</td>
+                    <td style="width:8%;">' . $item['Debit_Acc'] . '</td>
+                    <td style="width:12%;">' . $item['Credit_Acc'] . '</td>
+                    <td style="width:12%;">' . $item['remarks'] . '</td>
+                    <td style="width:10%;">' . $item['Amount'] . '</td>
                     <td style="width:12%;">' . $item['Amount'] . '</td>
+                    <td style="width:10%;">' . $item['Amount'] . '</td>
+                    <td style="width:12%;">' . $item['Amount'] . '</td>
+
                 </tr>';
             
             $totalAmount += $item['Amount']; // Accumulate total quantity
@@ -141,7 +145,7 @@ class RptCommissionsController extends Controller
         $pdf->SetXY(155, $currentY + 5);
         $pdf->MultiCell(20, 5, 'Total', 1, 'C');
         $pdf->SetXY(175, $currentY + 5);
-        $pdf->MultiCell(20, 5, $totalAmount, 1, 'C');
+        $pdf->MultiCell(25, 5, $totalAmount, 1, 'C');
     
         // // Prepare filename for the PDF
         $fromDate = Carbon::parse($request->fromDate)->format('Y-m-d');
