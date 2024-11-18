fb9a6292d1717e7afa9f5312d2540b165e61c39b
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-11-18 23:24:55 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -259,12 +259,6 @@
 						</div>
 
 						<div class="col-12 col-md-3 mb-2">
-							
-
-							
-						</div>
-
-						<div class="col-12 col-md-3 mb-2">
 							<section class="card card-featured-left card-featured-tertiary">
 								<div class="card-body"  >
 									<h3 class="amount text-dark"><strong>Long Term Loan</strong></h3>
