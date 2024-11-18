b6e00e4cba937623ee4380e87440b6ef72924cba
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-11-19 01:05:24 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -156,7 +156,7 @@
 							</section>
 						</div>
 
-						<div class="col-12 col-md-6 mb-2">
+						<div class="col-12 col-md-6 mb-2 mt-3">
 							<section class="card card-featured-left card-featured-success">
 								<div class="card-body">
 									<h3 class="amount text-dark"><strong>Active Users</strong></h3>
