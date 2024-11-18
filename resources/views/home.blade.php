8ad38799354ef87287fad7cdf7e59432fc1ddd30
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-18 17:28:22 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,10 +97,7 @@
 						<div class="col-12 col-md-3 mb-2">
 							<section class="card card-featured-left card-featured-primary mb-2">
 								<div class="card-body">
-									<div class="small-chart float-right" id="sparklineBar"><canvas width="79" height="50" style="display: inline-block; width: 79px; height: 50px; vertical-align: top;"></canvas></div>
-									<script type="text/javascript">
-										var sparklineBarData = [5, 6, 7, 2, 0, 4 , 2, 4, 2, 0, 4 , 2, 4, 2, 0, 4];
-									</script>
+									<strong class="amount">PDC</strong>
 									<div class="h4 font-weight-bold mb-0">488</div>
 									<p class="text-3 text-muted mb-0">Nullam quis ris</p>
 								</div>
