4d1dee6a2608a59fcbbbcdb252a0a49d193aedef
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-18 17:21:55 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,6 +93,37 @@
 					<!-- start: page -->
 					<div class="row cust-pad">
 						<!-- summaries -->
+
+						<div class="col-12 col-md-5 mb-2">
+							<section class="card card-featured-left card-featured-secondary mb-2">
+								<div class="card-body">
+									<div class="widget-summary">
+										
+										<div class="row">
+											<div class="summary col-6">
+												<strong class="amount">PDC</strong>
+												<div class="info">
+													@if(isset($pdc) && isset($pdc->Total_Balance) && strpos($pdc->Total_Balance, '.') !== false && substr($pdc->Total_Balance, strpos($pdc->Total_Balance, '.') + 1) > '0')
+														<h4 class="amount m-0 text-primary"><strong class="rolling-number amount m-0 text-primary" data-target="{{ number_format($pdc->Total_Balance, 0, '.', ',') }}">0</strong><span class="title text-end text-dark"> PKR</span></h4>
+													@elseif(isset($pdc) && isset($pdc->Total_Balance))
+														<h4 class="amount m-0 text-primary"><strong class="rolling-number amount m-0 text-primary" data-target="{{ number_format($pdc->Total_Balance, 0, '.', ',') }}" >0</strong><span class="title text-end text-dark"> PKR</span></h4>
+													@else
+														<h4 class="amount m-0 text-primary"><strong>0</strong><span class="title text-end text-dark"> PKR</span></h4>
+													@endif
+												</div>
+											</div>
+
+										</div>
+										<div>
+											<div class="summary-footer">
+												<a class="text-muted text-uppercase" href="#">(View Details)</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</section>
+						</div>
+
 						<div class="col-12 col-md-5 mb-2">
 							<section class="card card-featured-left card-featured-secondary mb-2">
 								<div class="card-body">
