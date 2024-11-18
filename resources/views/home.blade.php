670aafbbf4ae64e6cf2a5aafbd87ed0fcf649efd
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-11-18 22:35:38 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -256,97 +256,28 @@
 									<table class="table table-responsive-md table-striped mb-0">
 										<thead>
 											<tr>
-												<th>#</th>
-												<th>Project</th>
-												<th>Status</th>
-												<th>Progress</th>
+												<th>User</th>
+												<th></th>
 											</tr>
 										</thead>
 										<tbody>
 											<tr>
-												<td>1</td>
-												<td>Porto - Responsive HTML5 Template</td>
-												<td><span class="badge badge-success">Success</span></td>
-												<td>
-													<div class="progress progress-sm progress-half-rounded m-0 mt-1 light">
-														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-															100%
-														</div>
-													</div>
-												</td>
+												<td>User Name Here</td>
+												<td><span class="badge badge-success">Active</span></td>
 											</tr>
 											<tr>
-												<td>2</td>
-												<td>Porto - Responsive Drupal 7 Theme</td>
-												<td><span class="badge badge-success">Success</span></td>
-												<td>
-													<div class="progress progress-sm progress-half-rounded m-0 mt-1 light">
-														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-															100%
-														</div>
-													</div>
-												</td>
+												<td>User Name Here</td>
+												<td><span class="badge badge-success">Active</span></td>
 											</tr>
 											<tr>
-												<td>3</td>
-												<td>Tucson - Responsive HTML5 Template</td>
-												<td><span class="badge badge-warning">Warning</span></td>
-												<td>
-													<div class="progress progress-sm progress-half-rounded m-0 mt-1 light">
-														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-															60%
-														</div>
-													</div>
-												</td>
+												<td>User Name Here</td>
+												<td><span class="badge badge-success">Active</span></td>
 											</tr>
 											<tr>
-												<td>4</td>
-												<td>Tucson - Responsive Business WordPress Theme</td>
-												<td><span class="badge badge-success">Success</span></td>
-												<td>
-													<div class="progress progress-sm progress-half-rounded m-0 mt-1 light">
-														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-															90%
-														</div>
-													</div>
-												</td>
+												<td>User Name Here</td>
+												<td><span class="badge badge-success">Active</span></td>
 											</tr>
-											<tr>
-												<td>5</td>
-												<td>Porto - Responsive Admin HTML5 Template</td>
-												<td><span class="badge badge-warning">Warning</span></td>
-												<td>
-													<div class="progress progress-sm progress-half-rounded m-0 mt-1 light">
-														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 45%;">
-															45%
-														</div>
-													</div>
-												</td>
-											</tr>
-											<tr>
-												<td>6</td>
-												<td>Porto - Responsive HTML5 Template</td>
-												<td><span class="badge badge-danger">Danger</span></td>
-												<td>
-													<div class="progress progress-sm progress-half-rounded m-0 mt-1 light">
-														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
-															40%
-														</div>
-													</div>
-												</td>
-											</tr>
-											<tr>
-												<td>7</td>
-												<td>Porto - Responsive Drupal 7 Theme</td>
-												<td><span class="badge badge-success">Success</span></td>
-												<td>
-													<div class="progress progress-sm progress-half-rounded mt-1 light">
-														<div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 95%;">
-															95%
-														</div>
-													</div>
-												</td>
-											</tr>
+											
 										</tbody>
 									</table>
 								</div>
