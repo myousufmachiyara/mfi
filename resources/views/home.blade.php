4c338448f19750ed2dd8cc924371f43abe9b2975
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-19 17:25:04 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -112,7 +112,7 @@
 							</section>
 
 							<section class="card card-featured-left card-featured-success mt-3">
-								<div class="card-body"  >
+								<div class="card-body icon-container" style="background-image: url('/assets/img/long-term-loan-icon.png'); ">
 									<h3 class="amount text-dark"><strong>Long Term Loan</strong></h3>
 									@if(isset($long_term_loan) && isset($long_term_loan->total_balance))
 										<h2 class="amount m-0 text-success"><strong class="amount m-0 text-success">{{ $long_term_loan->total_balance }}</strong><span class="title text-end text-dark h6"> PKR</span></h2>
@@ -128,7 +128,7 @@
 
 						<div class="col-12 col-md-2 mb-2">
 							<section class="card card-featured-left card-featured-tertiary mb-2">
-								<div class="card-body icon-container"  style="background-image: url('/assets/img/fc-icon.png'); ">
+								<div class="card-body icon-container" style="background-image: url('/assets/img/fc-icon.png'); ">
 									<h3 class="amount text-dark"><strong>Foreign Currency</strong></h3>
 									@if(isset($foreign) && isset($foreign->Total_Balance))
 										<h2 class="amount m-0 text-tertiary"><strong>{{ $foreign->Total_Balance }}</strong><span class="title text-end text-dark h6"> PKR</span></h2>
