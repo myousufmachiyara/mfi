9223b0aa9e3a2b4a335e516f7c173a533b30e571
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-19 15:47:46 +0500

    Update RptGoDownItemGroupController.php

--- app/Http/Controllers/RptGoDownItemGroupController.php
+++ app/Http/Controllers/RptGoDownItemGroupController.php
@@ -177,11 +177,11 @@ class RptGoDownItemGroupController extends Controller
             <table border="1" style="border-collapse: collapse; text-align: center;">
                 <tr>
                     <th style="width:10%;color:#17365D;font-weight:bold;">S/No.</th>
-                    <th style="width:10%;color:#17365D;font-weight:bold;">Voucher #</th>
+                    <th style="width:12%;color:#17365D;font-weight:bold;">Voucher #</th>
                     <th style="width:25%;color:#17365D;font-weight:bold;">Item Name</th>
                     <th style="width:25%;color:#17365D;font-weight:bold;">Party Name</th>
-                    <th style="width:15%;color:#17365D;font-weight:bold;">Quantity</th>
-                    <th style="width:15%;color:#17365D;font-weight:bold;">Weight</th>
+                    <th style="width:14%;color:#17365D;font-weight:bold;">Quantity</th>
+                    <th style="width:14%;color:#17365D;font-weight:bold;">Weight</th>
                 </tr>';
     
         // Iterate through items and add rows
@@ -194,11 +194,11 @@ class RptGoDownItemGroupController extends Controller
             $html .= '
                 <tr style="background-color:' . $backgroundColor . ';">
                     <td style="width:10%;">' . $count . '</td>
-                    <td style="width:10%;">' . $item['Sal_inv_no'] . '</td>
+                    <td style="width:12%;">' . $item['Sal_inv_no'] . '</td>
                     <td style="width:25%;">' . $item['item_name'] . '</td>
                     <td style="width:25%;">' . $item['ac_name'] . '</td>
-                    <td style="width:15%;">' . $item['Sales_qty'] . '</td>
-                    <td style="width:15%;">' . $item['wt'] . '</td>
+                    <td style="width:14%;">' . $item['Sales_qty'] . '</td>
+                    <td style="width:14%;">' . $item['wt'] . '</td>
                 </tr>';
             
             $count++;
