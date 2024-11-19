193f8cf75b8cb55363e1ba8fec7299bf114f8c28
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-19 18:12:35 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,7 +36,7 @@
 					<!-- start: page -->
 					<div class="row cust-pad">
 						<div style="display: flex;justify-content: space-between;">
-							<h3 class="text-dark"><strong>Good Morning! Have a Nice Day</strong></h3>
+							<h2 class="text-dark"><strong>Good Morning! Have a Nice Day</strong></h2>
 							<div class="form-check form-switch">
 								<input class="form-check-input" type="checkbox" value="0" id="PurtoggleSwitch">
 							</div>
