d3519be8cad43f86aec5592cfd959e66dd63a2d7
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-20 12:58:30 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
 					@include('layouts.homepageheader')
 					<!-- start: page -->
 					<div class="row cust-pad">
-						@if(session['user_role']==1 || session['user_role']==2)
+						@if(session(['user_role'])==1 || session(['user_role'])==2)
 							<div style="display: flex;justify-content: space-between;">
 								<h2 class="text-dark"><strong>Good Morning! Have a Nice Day</strong></h2>
 								<div class="form-check form-switch">
