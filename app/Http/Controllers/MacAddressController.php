4f4df09208c509b464dddfd61510fd941aeb840f
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-21 14:41:14 +0500

    Update MacAddressController.php

--- app/Http/Controllers/MacAddressController.php
+++ app/Http/Controllers/MacAddressController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
 
 class MacAddressController extends Controller
 {
@@ -12,6 +13,39 @@ class MacAddressController extends Controller
         return view('mac_address', compact('macAddress'));
     }
 
+    // private function getMacAddress()
+    // {
+    //     try {
+    //         $macAddress = null;
+
+    //         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    //             // Windows system
+    //             $output = shell_exec('getmac');
+    //             if ($output) {
+    //                 $lines = explode("\n", trim($output));
+    //                 foreach ($lines as $line) {
+    //                     if (strpos($line, 'Physical Address') === false && preg_match('/([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}/', $line, $matches)) {
+    //                         $macAddress = $matches[0];
+    //                         break;
+    //                     }
+    //                 }
+    //             }
+    //         } else {
+    //             // Unix-based system (Linux, macOS)
+    //             $output = shell_exec('ifconfig -a');
+    //             if ($output) {
+    //                 preg_match('/([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}/', $output, $matches);
+    //                 $macAddress = $matches[0] ?? null;
+    //             }
+    //         }
+
+    //         return $macAddress ?: 'MAC address not found';
+    //     } catch (\Exception $e) {
+    //         \Log::error('Error fetching MAC address: ' . $e->getMessage());
+    //         return 'Error fetching MAC address';
+    //     }
+    // }
+
     private function getMacAddress()
     {
         try {
@@ -19,23 +53,18 @@ class MacAddressController extends Controller
 
             if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
                 // Windows system
-                $output = shell_exec('getmac');
-                if ($output) {
-                    $lines = explode("\n", trim($output));
-                    foreach ($lines as $line) {
-                        if (strpos($line, 'Physical Address') === false && preg_match('/([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}/', $line, $matches)) {
-                            $macAddress = $matches[0];
-                            break;
-                        }
-                    }
-                }
+                $process = new Process(['getmac']);
             } else {
                 // Unix-based system (Linux, macOS)
-                $output = shell_exec('ifconfig -a');
-                if ($output) {
-                    preg_match('/([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}/', $output, $matches);
-                    $macAddress = $matches[0] ?? null;
-                }
+                $process = new Process(['ifconfig', '-a']);
+            }
+
+            $process->run();
+
+            if ($process->isSuccessful()) {
+                $output = $process->getOutput();
+                preg_match('/([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}/', $output, $matches);
+                $macAddress = $matches[0] ?? null;
             }
 
             return $macAddress ?: 'MAC address not found';
