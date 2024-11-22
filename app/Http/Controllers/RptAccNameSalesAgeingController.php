2310ba26e472f1d771247717a0754a35e269c183
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-22 17:57:36 +0500

    Update RptAccNameSalesAgeingController.php

--- app/Http/Controllers/RptAccNameSalesAgeingController.php
+++ app/Http/Controllers/RptAccNameSalesAgeingController.php
@@ -97,6 +97,8 @@ class RptAccNameSalesAgeingController extends Controller
             foreach ($sales_days as $items) {
                 $bgColor = ($count % 2 == 0) ? '#f1f1f1' : '#ffffff';
                 $status = $items['remaining_amount'] == 0 ? 'Cleared' : 'Not Cleared';  // Determine the status here
+                $statusClass = $items['remaining_amount'] != 0 ? 'text-danger' : '';  // Add the class if remaining_amount is not 0
+                
                 $html .= "<tr style='background-color:{$bgColor};'>
                             <td>{$count}</td>
                             <td>" . Carbon::createFromFormat('Y-m-d', $items['bill_date'])->format('d-m-y') . "</td>
@@ -108,16 +110,13 @@ class RptAccNameSalesAgeingController extends Controller
                             <td>" . number_format($items['21_35_Days'], 0) . "</td>
                             <td>" . number_format($items['36_50_Days'], 0) . "</td>
                             <td>" . number_format($items['over_50_Days'], 0) . "</td>
-                            <td>{$items['max_days']} - {$status}</td>
+                            <td class='{$statusClass}'>{$items['max_days']} - {$status}</td>  <!-- Apply the class here -->
                         </tr>";
                 $count++;
             }
             $html .= '</table>';
-            
             $pdf->writeHTML($html, true, false, true, false, '');
-            
-            
-        
+                    
     
             // Filename and Output
         $filename = "Sales_Ageing_report_{$sales_days[0]['ac_name']}_from_{$formattedFromDate}_to_{$formattedToDate}.pdf";
