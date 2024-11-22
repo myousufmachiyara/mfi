302976ce00bd83a5b54598fe8c60b712ed0648ef
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-22 18:11:56 +0500

    Update RptAccNameSalesAgeingController.php

--- app/Http/Controllers/RptAccNameSalesAgeingController.php
+++ app/Http/Controllers/RptAccNameSalesAgeingController.php
@@ -98,6 +98,7 @@ class RptAccNameSalesAgeingController extends Controller
                 $bgColor = ($count % 2 == 0) ? '#f1f1f1' : '#ffffff';
                 $status = $items['remaining_amount'] == 0 ? 'Cleared' : 'Not Cleared';  // Determine the status here
                 $statusColor = ($items['remaining_amount'] != 0) ? 'color: red;' : '';  // Red color if remaining_amount is not 0
+                
                 $html .= "<tr style='background-color:{$bgColor};'>
                             <td>{$count}</td>
                             <td>" . Carbon::createFromFormat('Y-m-d', $items['bill_date'])->format('d-m-y') . "</td>
@@ -109,12 +110,13 @@ class RptAccNameSalesAgeingController extends Controller
                             <td>" . number_format($items['21_35_Days'], 0) . "</td>
                             <td>" . number_format($items['36_50_Days'], 0) . "</td>
                             <td>" . number_format($items['over_50_Days'], 0) . "</td>
-                            <td style='{$statusColor}'>{$items['max_days']} - {$status}</td>
+                            <td style='" . ($items['remaining_amount'] != 0 ? "color: red;" : "") . "'>{$items['max_days']} - {$status}</td>
                         </tr>";
                 $count++;
             }
             $html .= '</table>';
             $pdf->writeHTML($html, true, false, true, false, '');
+            
             
             
     
