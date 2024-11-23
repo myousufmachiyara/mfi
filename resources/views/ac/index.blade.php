4060de48372f0dcc8fefebb0d5326048b97a9c3e
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-23 14:30:27 +0500

    Update index.blade.php

--- resources/views/ac/index.blade.php
+++ resources/views/ac/index.blade.php
@@ -419,7 +419,7 @@
     var table = document.getElementById('cust-datatable-default');
 
     $('#columnSearch').on('keyup change', function() {
-        alert(this);
+        alert("hello");
         var columnIndex = $('#columnSelect').val(); // Get selected column index
         table.column(columnIndex).search(this.value).draw(); // Apply search and redraw
     });
