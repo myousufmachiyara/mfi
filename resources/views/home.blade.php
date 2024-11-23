1e290e7f6a9a61c721cf6b41e55ed2b4a3127f7e
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-23 17:17:04 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,7 +43,7 @@
 							<div class="col-12 col-md-3 mb-2">
 								<section class="card card-featured-left card-featured-primary">
 									<div class="card-body icon-container data-container" style="background-image: url('/assets/img/cheque-icon.png'); ">
-										<h3 class="amount text-dark"><strong>Post Date Cheques</strong></h3>
+										<h3 class="amount text-dark"><strong>Post Dated Cheques</strong></h3>
 										@if(isset($pdc) && isset($pdc->Total_Balance))
 											<h2 class="amount m-0 text-primary actual-data">
 												<strong data-value="{{ $pdc->Total_Balance }}">0</strong>
