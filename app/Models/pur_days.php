94489a564ebe0b305a1bdf2578b6562f4eea7f14
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-23 14:35:46 +0500

    pur_ageing

---
+++ app/Models/pur_days.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pur_days extends Model
+{
+    use HasFactory;
+    protected $table = "pur_days";
+    protected $fillable = [
+
+    ];
+}
