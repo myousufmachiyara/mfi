4d15a4fefdee44bee826f1e5daaf24df44bbada6
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-25 19:03:19 +0500

    Update RptAccGrpBAController.php

--- app/Http/Controllers/RptAccGrpBAController.php
+++ app/Http/Controllers/RptAccGrpBAController.php
@@ -124,7 +124,7 @@ class RptAccGrpBAController extends Controller
         
                 // Add sub-total row for this sub-header
                 $html .= '<tr style="background-color:#d9edf7; font-weight:bold;">
-                            <td colspan="4" style="text-align:right;">Sub Total:</td>
+                            <td colspan="4" style="text-align:right;"><strong>Sub Total For ' . $subHeadCount . '</strong></td>
                             <td>' . number_format($subTotalDebit, 0) . '</td>
                             <td>' . number_format($subTotalCredit, 0) . '</td>
                           </tr>';
