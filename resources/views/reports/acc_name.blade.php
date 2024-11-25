9605f1a78fc89c1a8469500d1ebb2378f59f1395
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-25 13:21:08 +0500

    Update acc_name.blade.php

--- resources/views/reports/acc_name.blade.php
+++ resources/views/reports/acc_name.blade.php
@@ -682,7 +682,22 @@
                             $.each(result['lager_much_all'], function(k, v) {
                                 var html = "<tr>";
                                 html += "<td>" + (k + 1) + "</td>";
-                                html += "<td>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</td>";
+                                // html += "<td><a href='/sales/saleinvoice/view/"+v['auto_lager']+"'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a></td>";
+                                if (v['entry_of'] === 'Sale') {
+                                    html += "<td><a href='/sales/saleinvoice/view/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
+                                } else if (v['entry_of'] === 'SP') {
+                                    html += "<td><a href='/sales2/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
+                                } else if (v['entry_of'] === 'Pur') {
+                                    html += "<td><a href='/purchase1/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
+                                } else if (v['entry_of'] === 'PP') {
+                                    html += "<td><a href='/purchase2/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
+                                } else if (v['entry_of'] === 'JV1') {
+                                    html += "<td><a href='/vouchers/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
+                                } else if (v['entry_of'] === 'JV2') {
+                                    html += "<td><a href='/vouchers2/print/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
+                                } else {
+                                    html += "<td>" + (v['entry_of'] ? v['entry_of'] : "") + " " + (v['prefix'] ? v['prefix'] : "") + "</td>";
+                                }
                                 html += "<td>" + (v['entry_of'] ? v['entry_of'] : "") + "</td>";
                                 html += "<td>" + (v['jv_date'] ? moment(v['jv_date']).format('DD-MM-YYYY') : "") + "</td>";
                                 html += "<td>" + (v['ac2'] ? v['ac2'] : "") + "</td>";
@@ -786,19 +801,19 @@
                                 html += "<td>" + (k + 1) + "</td>";
                                 // html += "<td><a href='/sales/saleinvoice/view/"+v['auto_lager']+"'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a></td>";
                                 if (v['entry_of'] === 'Sale') {
-                                    html += "<td><a href='/sales/saleinvoice/view/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a></td>";
+                                    html += "<td><a href='/sales/saleinvoice/view/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
                                 } else if (v['entry_of'] === 'SP') {
-                                    html += "<td><a href='/sales2/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a></td>";
+                                    html += "<td><a href='/sales2/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
                                 } else if (v['entry_of'] === 'Pur') {
-                                    html += "<td><a href='/purchase1/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a></td>";
-                                }  else if (v['entry_of'] === 'PP') {
-                                    html += "<td><a href='/purchase2/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a></td>";
+                                    html += "<td><a href='/purchase1/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
+                                } else if (v['entry_of'] === 'PP') {
+                                    html += "<td><a href='/purchase2/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
                                 } else if (v['entry_of'] === 'JV1') {
-                                    html += "<td><a href='/vouchers/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a></td>";
+                                    html += "<td><a href='/vouchers/show/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
                                 } else if (v['entry_of'] === 'JV2') {
-                                    html += "<td><a href='/vouchers2/print/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a></td>";
-                                }  else {
-                                    html += "<td>" + (v['entry_of'] ? v['entry_of'] : "") + "</td>";
+                                    html += "<td><a href='/vouchers2/print/"+v['auto_lager']+"' target='_blank'>" + (v['auto_lager'] ? v['auto_lager'] : "") + "</a> " + (v['prefix'] ? v['prefix'] : "") + "</td>";
+                                } else {
+                                    html += "<td>" + (v['entry_of'] ? v['entry_of'] : "") + " " + (v['prefix'] ? v['prefix'] : "") + "</td>";
                                 }
 
                                 html += "<td>" + (v['entry_of'] ? v['entry_of'] : "") + "</td>";
