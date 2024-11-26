4acf7306f346aa9d066a00828c7823649f01709a
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-26 16:25:19 +0500

    Update view.blade.php

--- resources/views/sales/view.blade.php
+++ resources/views/sales/view.blade.php
@@ -1,9 +1,9 @@
 @include('../layouts.header')
 	<body>
 		<section class="body">
-			<div class="inner-wrapper cust-pad">
-				<section role="main" class="content-body" style="margin:0px" >
-				@include('../layouts.pageheader')
+			<div class="inner-wrapper">
+				<section role="main" class="content-body" style="margin:0px;padding:75px 10px !important">
+					@include('../layouts.pageheader')
 
                     <section class="card">
 
@@ -13,11 +13,11 @@
 
 								<header class="clearfix">
 									<div class="row">
-										<div class="col-sm-6 mt-3">
+										<div class="col-8 mt-3 mb-3">
 											<h2 class="h2 mt-0 mb-1" style="color:#17365D">Sale INVOICE NO:</h2>
 											<h4 class="h4 m-0 text-dark font-weight-bold">{{$sales->prefix}}{{$sales->Sal_inv_no}}</h4>
 										</div>
-										<div class="col-sm-6 text-end mt-3 mb-3">
+										<div class="col-4 text-end mt-3 mb-3">
 											<div class="ib">
 												<img width="100px" src="/assets/img/logo.png" alt="MFI Logo" />
 											</div>
@@ -112,59 +112,53 @@
 								</table>
 								</table>
 
-								<div class="row">
-									<div class="col-8">
-										<div class="row">
-											<div class="col-6">
-												<table class="table h6 text-dark">
-													<tbody>
-														<tr class="b-top-0">
-															<td colspan="2"  style="color:#17365D">Total Quantity</td>
-															<td class="text-left">{{$total_quantity}}</td>
-														</tr>
-														<tr>
-															<td colspan="2"  style="color:#17365D">Total Weight(KGs)</td>
-															<td class="text-left">{{$total_weight}}</td>
-														</tr>
-													</tbody>
-												</table>
-												<h3 style="color:#17365D; text-decoration: underline;" id="numberInWords"></h3>
-
-											</div>
-										</div>
+								<div class="row" style="justify-content: space-between">
+									<div class="col-12 col-md-4">
+										<table class="table h6 text-dark">
+											<tbody>
+												<tr class="b-top-0">
+													<td colspan="2"  style="color:#17365D">Total Quantity</td>
+													<td class="text-left">{{$total_quantity}}</td>
+												</tr>
+												<tr>
+													<td colspan="2"  style="color:#17365D">Total Weight(KGs)</td>
+													<td class="text-left">{{$total_weight}}</td>
+												</tr>
+											</tbody>
+										</table>
+										<h3 style="color:#17365D; text-decoration: underline;" id="numberInWords"></h3>
 									</div>
-									<div class="col-4 invoice-summary">
-										<div class="row justify-content-end">
-											<table class="table h6 text-dark">
-												<tbody>
-													<tr class="b-top-0">
-														<td colspan="2"  style="color:#17365D" >Subtotal</td>
-														<td class="text-left">{{$subtotal}}</td>
-													</tr>
-													<tr>
-														<td colspan="2"  style="color:#17365D">Labour Charges</td>
-														<td class="text-left">{{$sales->LaborCharges}} PKR</td>
-													</tr>
-														<td colspan="2"  style="color:#17365D">Convance Charges</td>
-														<td class="text-left">{{$sales->ConvanceCharges}} PKR</td>
-													</tr>
-													</tr>
-														<td colspan="2"  style="color:#17365D">Discount</td>
-														<td class="text-left">{{$sales->Bill_discount}} PKR</td>
-													</tr>
-													<?php $netamount=round($subtotal + $sales->LaborCharges + $sales->ConvanceCharges - $sales->Bill_discount) ?>
-													<tr class="h5">
-														<td colspan="2"  style="color:#17365D">Net Amount</td>
-														<td class="text-left text-danger" style="font-weight:700">{{number_format($netamount)}} PKR</td>
-													</tr>
-												</tbody>
-											</table>
-										</div>
+
+									<div class="col-12 col-md-4">
+										<table class="table h6 text-dark">
+											<tbody>
+												<tr class="b-top-0">
+													<td colspan="2"  style="color:#17365D" >Subtotal</td>
+													<td class="text-left">{{$subtotal}}</td>
+												</tr>
+												<tr>
+													<td colspan="2"  style="color:#17365D">Labour Charges</td>
+													<td class="text-left">{{$sales->LaborCharges}} PKR</td>
+												</tr>
+													<td colspan="2"  style="color:#17365D">Convance Charges</td>
+													<td class="text-left">{{$sales->ConvanceCharges}} PKR</td>
+												</tr>
+												</tr>
+													<td colspan="2"  style="color:#17365D">Discount</td>
+													<td class="text-left">{{$sales->Bill_discount}} PKR</td>
+												</tr>
+												<?php $netamount=round($subtotal + $sales->LaborCharges + $sales->ConvanceCharges - $sales->Bill_discount) ?>
+												<tr class="h5">
+													<td colspan="2"  style="color:#17365D">Net Amount</td>
+													<td class="text-left text-danger" style="font-weight:700">{{number_format($netamount)}} PKR</td>
+												</tr>
+											</tbody>
+										</table>
 									</div>
 								<div>
 							</div>
 
-							<div class="d-grid gap-3 d-md-flex justify-content-md-end me-4">
+							<div class="text-end">
 								<a onclick="window.location='{{ route('all-saleinvoices') }}'" class="btn btn-primary mt-2 mb-2"> <i class="fas fa-arrow-left"></i> Back</a>
 								<a href="{{ route('print-sale-invoice', $sales->Sal_inv_no) }}" class="btn btn-danger mt-2 mb-2" target="_blank"> <i class="fas fa-print"></i> Print</a>
 							</div>
