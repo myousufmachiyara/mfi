d0a064d6234718ad53e113c29f8e2bc7a1ef4f07
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-26 13:57:43 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -508,13 +508,6 @@
 				}
 				return numbers;
 			},
-			CHART_COLORS: {
-				red: 'rgba(255, 99, 132, 0.2)',
-				blue: 'rgba(54, 162, 235, 0.2)',
-				green: 'rgba(75, 192, 192, 0.2)',
-				black: 'rgba(0, 0, 0, 0.2)',
-				yellow: 'rgba(255, 159, 64, 0.2)',
-			}
 		};
 
 		$(document).ready(function() {
