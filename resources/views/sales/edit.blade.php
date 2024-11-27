74edcb6df5a66c49ed4bdd03b860b50df5f820b8
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-27 14:37:34 +0500

    Update edit.blade.php

--- resources/views/sales/edit.blade.php
+++ resources/views/sales/edit.blade.php
@@ -189,8 +189,7 @@
 									<footer class="card-footer">
 										<div class="row form-group mb-2">
 											<div class="text-end">
-												<button type="button" class="btn btn-warning mt-2"  onclick="window.location='{{ route('all-saleinvoices') }}'"> <i class="fas fa-trash"></i> Discard Changes</button>
-												<button class="btn btn-danger mt-2"> <i class="fas fa-print"></i> Print Preview</button>
+												<button type="button" class="btn btn-danger mt-2"  onclick="window.location='{{ route('all-saleinvoices') }}'"> <i class="fas fa-trash"></i> Discard Changes</button>
 												<button type="submit" class="btn btn-primary mt-2"> <i class="fas fa-save"></i> Save Invoice</button>
 											</div>
 										</div>
