037f1f5c92807adcc3c3a41c76a65fda0c74dfe2
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-27 16:14:08 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -419,6 +419,7 @@
 										<div class="form-group">
 											<label class="col-form-label">Select Month</label>
 											<input type="month" class="form-control"  value="{{ date('Y-m') }}">
+											<a class="btn btn-primary" style="margin-top: 2.1rem;padding: 0.5rem 0.6rem;"><i class="fa fa-filter"></i></a>
 										</div>
 									</div>
 									<div class="row form-group pb-3">
