23d9929dadc800c1bd1afe36c699c0fadc239842
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-27 12:26:10 +0500

    Update view.blade.php

--- resources/views/tpo/view.blade.php
+++ resources/views/tpo/view.blade.php
@@ -132,7 +132,7 @@
 									@php($grand_subtotal += $subtotal)
 									@endforeach
 								
-									<div class="row d-none">
+									<div class="row">
 										<div class="col-12 col-md-8">
 											<table class="table h6 text-dark">
 												<tbody>
