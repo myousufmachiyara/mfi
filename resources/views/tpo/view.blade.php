7ea1b7e8f9a64580858ba0ff84ec7a5f75f0a078
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-27 12:21:22 +0500

    Update view.blade.php

--- resources/views/tpo/view.blade.php
+++ resources/views/tpo/view.blade.php
@@ -1,19 +1,19 @@
 @include('../layouts.header')
 	<body>
 		<section class="body">
-			<div class="inner-wrapper cust-pad">
-				<section role="main" class="content-body" style="margin:0px" >
-				@include('../layouts.pageheader')
+			<div class="inner-wrapper">
+				<section role="main" class="content-body" style="margin:0px;padding:75px 10px !important">
+					@include('../layouts.pageheader')
                     <section class="card">
 						<div class="card-body">
 							<div class="invoice">
 								<header class="clearfix">
 									<div class="row">
-										<div class="col-sm-6 mt-3">
+										<div class="col-8 mt-3 mb-3">
 											<h2 class="h2 mt-0 mb-1" style="color:#17365D">PURCHASE ORDER NO:</h2>
 											<h4 class="h4 m-0 text-dark font-weight-bold">{{$pur->prefix}}{{$pur->Sale_inv_no}}</h4>
 										</div>
-										<div class="col-sm-6 text-end mt-3 mb-3">
+										<div class="col-4 text-end mt-3 mb-3">
 											<div class="ib">
 												<img width="100px" src="/assets/img/logo.png" alt="MFI Logo" />
 											</div>
@@ -49,8 +49,6 @@
 										</div>
 										<div class="col-md-5">
 											<div class="bill-data">
-
-
 												<h4 class="mb-0 h6 mb-1 text-dark font-weight-semibold">
 													<span  style="color:#17365D">Purchase Inv No: &nbsp</span>
 													<span style="font-weight:400;color:black" class="value"> {{$pur->sales_against}}</span>
@@ -127,8 +125,6 @@
 												<td colspan="3"></td>
 												<td class="text-center text-danger font-weight-bold" >{{ $subtotal }}</td> <!-- Subtotal Amount -->
 											</tr>
-											
-											
 										</tbody>
 									</table>
 									@php($grand_total_quantity += $subtotal_quantity)
@@ -137,75 +133,63 @@
 									@endforeach
 								
 									<div class="row">
-										<div class="col-8">
-											<div class="row">
-												<div class="col-6">
-													<table class="table h6 text-dark">
-														<tbody>
-															<tr class="b-top-0">
-																<td colspan="2"  style="color:#17365D">Total Quantity</td>
-																<td class="text-left">{{$grand_total_quantity}}</td>
-															</tr>
-															<tr>
-																<td colspan="2"  style="color:#17365D">Total Weight(KGs)</td>
-																<td class="text-left">{{$grand_total_weight}}</td>
-															</tr>
-														</tbody>
-													</table>
-													<h3 style="color:#17365D; text-decoration: underline;" id="numberInWords"></h3>
-												</div>
-												
-												<div>
-													<h2 class="mb-0 h6 mb-1 text-dark font-weight-semibold">
-													<span style="color:#17365D; font-size:20px; font-weight:bold; font-style:italic; text-decoration:underline;">
-														Terms And Conditions:&nbsp
-													</span><br><br>
-													<span style="font-weight:400;color:rgb(224, 8, 8);white-space: pre-wrap;word-wrap: break-word;" class="value">{{ htmlspecialchars($pur->tc) }}</span>
-													</h2>
-												</div>
-											</div>
+										<div class="col-12 col-md-8">
+											<table class="table h6 text-dark">
+												<tbody>
+													<tr class="b-top-0">
+														<td colspan="2"  style="color:#17365D">Total Quantity</td>
+														<td class="text-left">{{$grand_total_quantity}}</td>
+													</tr>
+													<tr>
+														<td colspan="2"  style="color:#17365D">Total Weight(KGs)</td>
+														<td class="text-left">{{$grand_total_weight}}</td>
+													</tr>
+												</tbody>
+											</table>
+											<h3 style="color:#17365D; text-decoration: underline;" id="numberInWords"></h3>
+										
+											<h2 class="mb-0 h6 mb-1 text-dark font-weight-semibold">
+											<span style="color:#17365D; font-size:20px; font-weight:bold; font-style:italic; text-decoration:underline;">
+												Terms And Conditions:&nbsp
+											</span><br><br>
+											<span style="font-weight:400;color:rgb(224, 8, 8);white-space: pre-wrap;word-wrap: break-word;" class="value">{{ htmlspecialchars($pur->tc) }}</span>
+											</h2>
 										</div>
-										<div class="col-4 invoice-summary">
-											<div class="row justify-content-end">
-												<table class="table h6 text-dark">
-													<tbody>
-														<tr class="b-top-0">
-															<td colspan="2"  style="color:#17365D" >Bill total</td>
-															<td class="text-left">{{$grand_subtotal}}</td>
-														</tr>
-														<tr>
-															<td colspan="2"  style="color:#17365D">Labour Charges</td>
-															<td class="text-left">{{$pur->LaborCharges}} PKR</td>
-														</tr>
-															<td colspan="2"  style="color:#17365D">Convance Charges</td>
-															<td class="text-left">{{$pur->ConvanceCharges}} PKR</td>
-														</tr>
-														</tr>
-															<td colspan="2"  style="color:#17365D">Discount</td>
-															<td class="text-left">{{$pur->Bill_discount}} PKR</td>
-														</tr>
-														<?php $netamount=round($grand_subtotal + $pur->LaborCharges + $pur->ConvanceCharges - $pur->Bill_discount) ?>
-														<tr class="h5">
-															<td colspan="2"  style="color:#17365D">Net Amount</td>
-															<td class="text-left text-danger" style="font-weight:700">{{number_format($netamount)}} PKR</td>
-														</tr>
-														
-														
-													</tbody>
-												</table>
-											</div>
+										<div class="col-12 col-md-4 invoice-summary">
+											<table class="table h6 text-dark">
+												<tbody>
+													<tr class="b-top-0">
+														<td colspan="2"  style="color:#17365D" >Bill total</td>
+														<td class="text-left">{{$grand_subtotal}}</td>
+													</tr>
+													<tr>
+														<td colspan="2"  style="color:#17365D">Labour Charges</td>
+														<td class="text-left">{{$pur->LaborCharges}} PKR</td>
+													</tr>
+														<td colspan="2"  style="color:#17365D">Convance Charges</td>
+														<td class="text-left">{{$pur->ConvanceCharges}} PKR</td>
+													</tr>
+													</tr>
+														<td colspan="2"  style="color:#17365D">Discount</td>
+														<td class="text-left">{{$pur->Bill_discount}} PKR</td>
+													</tr>
+													<?php $netamount=round($grand_subtotal + $pur->LaborCharges + $pur->ConvanceCharges - $pur->Bill_discount) ?>
+													<tr class="h5">
+														<td colspan="2"  style="color:#17365D">Net Amount</td>
+														<td class="text-left text-danger" style="font-weight:700">{{number_format($netamount)}} PKR</td>
+													</tr>
+													
+													
+												</tbody>
+											</table>
 										</div>
-										<div>
+										<div class="col-12 text-end">
+											<a onclick="window.location='{{ route('all-tpo') }}'" class="btn btn-primary mt-2 mb-2"> <i class="fas fa-arrow-left"></i> Back</a>
+											<a class="btn btn-danger mt-2 mb-2 mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal" onclick="setPrintId({{$pur->Sale_inv_no}})" href="#printModal"><i class="fas fa-print"></i> Print</a>
+										</div>		
 									</div>
 								</div>
-								
-								<div class="d-grid gap-3 d-md-flex justify-content-md-end me-4">
-								<a onclick="window.location='{{ route('all-tpo') }}'" class="btn btn-primary mt-2 mb-2"> <i class="fas fa-arrow-left"></i> Back</a>
-								<a class="btn btn-danger mt-2 mb-2 mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal" onclick="setPrintId({{$pur->Sale_inv_no}})" href="#printModal"><i class="fas fa-print"></i> Print</a>
-								</div>
-
 							</div>
-
 
 							<div id="printModal" class="zoom-anim-dialog modal-block modal-block-danger mfp-hide" style="max-width: 350px;">
 								<form method="get" action="{{ route('print-tpo-invoice') }}" target="_blank" enctype="multipart/form-data">
@@ -237,10 +221,9 @@
 									</section>
 								</form>
 							</div>
-
-						</section>
+						</div>
 					</section>
-				</div>
+				</section>
 			</div>
 		</section>
         @include('../layouts.footerlinks')
