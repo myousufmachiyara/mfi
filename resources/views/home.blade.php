c5e86858fb553d8a1a56e52d1848a4f814390033
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-29 16:31:14 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -454,7 +454,7 @@
 												</header>
 												<div class="card-body">
 												<div class="row mb-2">
-													<div class="col-lg-3">
+													<div class="col-lg-6">
 														<div class="form-group">
 														
 															<select data-plugin-selecttwo class="form-control select2-js" id="hr_monthly_tonage_of_coa" name="account_name" required onchange="getMonthlyTonageOfCustomer()">
