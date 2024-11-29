669daac5dbbbb5dc04ee232a2653cc5f45450d2e
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-29 16:29:39 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -453,7 +453,7 @@
 													<h2 class="card-title">Monthly Tonage Of Customer From Purchase 2</h2>
 												</header>
 												<div class="card-body">
-													<div class="col-sm-12 col-md-8 mb-3">
+													<div class="col-lg-3">
 														<div class="form-group">
 														
 															<select data-plugin-selecttwo class="form-control select2-js" id="hr_monthly_tonage_of_coa" name="account_name" required onchange="getMonthlyTonageOfCustomer()">
