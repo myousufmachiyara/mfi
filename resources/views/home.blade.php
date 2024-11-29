ab8a4e85a5978b73c7f5595ec4e9f47e99db798a
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-29 17:06:43 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -487,12 +487,15 @@
 										</div>
 
 										<div class="mb-3 text-end">
-											<div class="form-group" style="display: inline-block">
-												{{-- <label class="col-form-label">Select Month</label> --}}
+											<div class="form-group d-inline-block">
+												<label for="filterHR" class="visually-hidden">Select Month</label>
 												<input type="month" class="form-control" id="filterHR" value="{{ date('Y-m') }}" onchange="getTabData()">
 											</div>
-											<a class="btn btn-primary" style="padding: 0.5rem 0.6rem;" onclick="getTabData()"><i class="fa fa-filter"></i></a>
+											<button class="btn btn-primary" style="padding: 0.5rem 0.6rem;" onclick="getTabData()" aria-label="Filter by month">
+												<i class="fa fa-filter"></i>
+											</button>
 										</div>
+										
 
 										<div class="col-12 col-md-3 mb-3">
 											<section class="card">
