66a0fb794852ee83da3aaf1530c78d047c3eae14
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-11-30 17:45:51 +0500

    IIL

---
+++ app/Http/Controllers/DashboardIILTabController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\dash_pur_2_summary_monthly_companywise;
+use App\Models\sale_pipe_summary_of_party_by_mill;
+use App\Models\pur_pipe_summary_of_party_by_mill;
+use App\Models\AC;
+use App\Models\top_customers_of_sale2;
+use App\Models\top_customers_of_pur2;
+
+class DashboardIILTabController extends Controller
+{
+    //
+}
