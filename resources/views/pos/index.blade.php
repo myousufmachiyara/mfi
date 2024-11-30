e2f5fc23583c3960f69e390ce96e3c14f495a867
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-11-30 10:13:05 +0500

    Update index.blade.php

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -4,6 +4,18 @@
 		    @include('../layouts.pageheader')
             <div class="inner-wrapper cust-pad">
                 <section role="main" class="content-body" style="margin:0px">
+                <div class="row">
+                    <div class="col-12 mb-3">								
+                        <section class="card">
+                            <header class="card-header" style="display: flex;justify-content: space-between;">
+                                <h2 class="card-title">New Invoice</h2>
+                                <div class="card-actions">
+                                    <button type="button" class="btn btn-primary" onclick="addNewRow_btn()"> <i class="fas fa-plus"></i> Add New Item </button>
+                                </div>
+                            </header>
+                        </section>
+                    </div>
+                </div>
                 </section>
             </div>
         </section>
