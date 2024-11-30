916e08d77c84486ad80f8259ed1d616ab0fb3af5
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-11-30 11:15:13 +0500

    Update index.blade.php

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -44,6 +44,34 @@
                                                 </div>
                                             </div>
                                         </div>
+                                        <div class="card card-modern card-modern-alt-padding col-5 mt-0">
+                                            <div class="card-body bg-light">
+                                                <div class="image-frame mb-2">
+                                                    <div class="image-frame-wrapper">
+                                                        <img src="/assets/img/empty-300x240.jpg" class="img-fluid" alt="Product Short Name">
+                                                    </div>
+                                                </div>
+                                                <h4 class="text-4 line-height-2 mt-0 mb-2"><a href="ecommerce-products-form.html" class="ecommerce-sidebar-link text-color-dark text-color-hover-primary text-decoration-none">Product Name</a></h4>
+                                                <div class="product-price">
+                                                    <div class="regular-price on-sale">$59.00</div>
+                                                    <div class="sale-price">$49.00</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card card-modern card-modern-alt-padding col-5 mt-0">
+                                            <div class="card-body bg-light">
+                                                <div class="image-frame mb-2">
+                                                    <div class="image-frame-wrapper">
+                                                        <img src="/assets/img/empty-300x240.jpg" class="img-fluid" alt="Product Short Name">
+                                                    </div>
+                                                </div>
+                                                <h4 class="text-4 line-height-2 mt-0 mb-2"><a href="ecommerce-products-form.html" class="ecommerce-sidebar-link text-color-dark text-color-hover-primary text-decoration-none">Product Name</a></h4>
+                                                <div class="product-price">
+                                                    <div class="regular-price on-sale">$59.00</div>
+                                                    <div class="sale-price">$49.00</div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="col-lg-8">
                                         <div class="card-body" style="overflow-x:auto;min-height:250px;max-height:450px;overflow-y:auto">
