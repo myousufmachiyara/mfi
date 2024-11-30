bc57db3d2d8f12d4acce906049132dc0d73f00e1
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-11-30 11:21:48 +0500

    Update index.blade.php

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -15,7 +15,7 @@
                             </header>
                             <div class="card-body">
                                 <div class="row">
-                                    <div class="col-lg-4">
+                                    <div class="col-lg-4 row">
                                         <div class="card card-modern card-modern-alt-padding col-5 mt-0">
                                             <div class="card-body bg-light">
                                                 <div class="image-frame mb-2">
