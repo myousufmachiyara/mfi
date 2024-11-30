e5e7aeeef52c692659af18d02bf01b45b33a418c
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2024-11-30 11:58:59 +0500

    Update index.blade.php

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -75,45 +75,96 @@
                                     </div>
                                     <div class="col-lg-9">
                                         <div class="card-body" style="overflow-x:auto;min-height:250px;max-height:450px;overflow-y:auto">
-                                            <table class="table table-bordered table-striped mb-0" id="myTable" >
-                                                <thead>
-                                                    <tr>
-                                                        <th width="15%">Item Code<span style="color: red;"><strong>*</strong></span></th>
-                                                        <th>Item Name<span style="color: red;"><strong>*</strong></span></th>
-                                                        <th width="15%">Qty<span style="color: red;"><strong>*</strong></span></th>
-                                                        <th width="15%">Price<span style="color: red;"><strong>*</strong></span></th>
-                                                        <th width="15%">Amount</th>
-                                                        <th></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody id="saleInvoiceTable">
-                                                    <tr>
-                                                        <td>
-                                                            <input type="number" id="item_code1" name="item_code[]" placeholder="Code" class="form-control" required onchange="getItemDetails(1,1)">
-                                                        </td>
-                                                        <td>
-                                                            <select class="form-control select2-js" id="item_name1" onchange="getItemDetails(1,2)" name="item_name[]" required>
-                                                            <option selected>Select Item</option>
-                                                               
-                                                            </select>
-                                                        </td>
-                                                        <td>
-                                                            <input type="number" id="item_qty1" name="item_qty[]" onchange="rowTotal(0)" placeholder="Qty" value="0" step="any" required class="form-control">
-                                                        </td>
-                                                        <td>
-                                                            <input type="number" id="price1" name="item_price[]" onchange="rowTotal(1)" placeholder="Price" value="0" step="any" required class="form-control">
-                                                        </td>
-                                                        <td>
-                                                            <input type="number" id="amount1" name="item_amount[]" placeholder="Amount" class="form-control" value="0" step="any" required disabled>
-                                                        </td>
-                                                        <td>
-                                                            <button type="button" onclick="removeRow(this)" class="btn btn-danger" tabindex="1"><i class="fas fa-times"></i></button>
-                                                        </td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
+                                            <form method="post" id="myForm" enctype="multipart/form-data" onkeydown="return event.key != 'Enter';">
+                                                @csrf
+                                                <table class="table table-bordered table-striped mb-0" id="myTable" >
+                                                    <thead>
+                                                        <tr>
+                                                            <th width="15%">Item Code<span style="color: red;"><strong>*</strong></span></th>
+                                                            <th>Item Name<span style="color: red;"><strong>*</strong></span></th>
+                                                            <th width="15%">Qty<span style="color: red;"><strong>*</strong></span></th>
+                                                            <th width="15%">Price<span style="color: red;"><strong>*</strong></span></th>
+                                                            <th width="15%">Amount</th>
+                                                            <th></th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody id="saleInvoiceTable">
+                                                        <tr>
+                                                            <td>
+                                                                <input type="number" id="item_code1" name="item_code[]" placeholder="Code" class="form-control" required onchange="getItemDetails(1,1)">
+                                                            </td>
+                                                            <td>
+                                                                <select class="form-control select2-js" id="item_name1" onchange="getItemDetails(1,2)" name="item_name[]" required>
+                                                                <option selected>Select Item</option>
+                                                                
+                                                                </select>
+                                                            </td>
+                                                            <td>
+                                                                <input type="number" id="item_qty1" name="item_qty[]" onchange="rowTotal(0)" placeholder="Qty" value="0" step="any" required class="form-control">
+                                                            </td>
+                                                            <td>
+                                                                <input type="number" id="price1" name="item_price[]" onchange="rowTotal(1)" placeholder="Price" value="0" step="any" required class="form-control">
+                                                            </td>
+                                                            <td>
+                                                                <input type="number" id="amount1" name="item_amount[]" placeholder="Amount" class="form-control" value="0" step="any" required disabled>
+                                                            </td>
+                                                            <td>
+                                                                <button type="button" onclick="removeRow(this)" class="btn btn-danger" tabindex="1"><i class="fas fa-times"></i></button>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </form>
 									    </div>
                                         <footer class="card-footer">
+                                            <div class="row form-group mb-3">
+                                                <div class="col-6 col-md-2 pb-sm-3 pb-md-0">
+                                                    <label class="col-form-label">Total Amount</label>
+                                                    <input type="number" id="total_amount_show" placeholder="Total Amount" class="form-control" step="any" disabled>
+                                                    <input type="hidden" id="totalAmount" name="totalAmount" step="any" placeholder="Total Amount" class="form-control">
+                                                </div>
+
+                                                <div class="col-6 col-md-2 pb-sm-3 pb-md-0">
+                                                    <label class="col-form-label">Total Weight</label>
+                                                    <input type="number" id="total_weight_show"  placeholder="Total Weight" class="form-control" step="any" disabled>
+                                                    <input type="hidden" id="total_weight" name="total_weight" step="any" placeholder="Total Weight" class="form-control">
+                                                </div>
+
+                                                <div class="col-6 col-md-2 pb-sm-3 pb-md-0">
+                                                    <label class="col-form-label">Total Quantity</label>
+                                                    <input type="number" id="total_quantity" name="total_quantity" placeholder="Total Weight" class="form-control" step="any" disabled>
+                                                </div>
+
+                                                <div class="col-6 col-md-2 pb-sm-3 pb-md-0">
+                                                    <label class="col-form-label">Convance Charges</label>
+                                                    <input type="number" id="convance_charges" onchange="netTotal()" name="convance_charges" placeholder="Convance Charges" step="any" value="0" class="form-control">
+                                                </div>
+
+                                                <div class="col-6 col-md-2 pb-sm-3 pb-md-0">
+                                                    <label class="col-form-label">Labour Charges</label>
+                                                    <input type="number" id="labour_charges"  onchange="netTotal()" name="labour_charges" placeholder="Labour Charges" step="any" value="0" class="form-control">
+                                                </div>
+
+                                                <div class="col-6 col-md-2 pb-sm-3 pb-md-0">
+                                                    <label class="col-form-label">Bill Discount</label>
+                                                    <input type="number" id="bill_discount"  onchange="netTotal()" name="bill_discount" placeholder="Bill Discount" step="any" value="0" class="form-control">
+                                                </div>
+
+                                            </div>
+                                            <div class="col-sm-12 col-md-12 pb-sm-3 pb-md-0 text-end">
+                                                <h3 class="font-weight-bold mt-3 mb-0 text-5 text-primary">Net Amount</h3>
+                                                <span>
+                                                    <strong class="text-4 text-primary">PKR <span id="netTotal" class="text-4 text-danger">0.00 </span></strong>
+                                                </span>
+                                            </div>
+									    </footer>
+                                        <footer class="card-footer">
+                                            <div class="row form-group mb-2">
+                                                <div class="text-end">
+                                                    <button type="button" class="btn btn-danger mt-2"  onclick="window.location='{{ route('all-saleinvoices') }}'"> <i class="fas fa-trash"></i> Discard Invoice</button>
+                                                    <button type="submit" class="btn btn-primary mt-2"> <i class="fas fa-save"></i> Add Invoice</button>
+                                                </div>
+                                            </div>
                                         </footer>
                                     </div>
                                 </div>
@@ -129,4 +180,124 @@
         </section>
         @include('../layouts.footerlinks')
 	</body>
+    <script>
+        var index=2;
+
+        document.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                addNewRow();	
+            }
+        });
+
+        function getItemDetails(row_no,option){
+            var itemId;
+            if(option==1){
+                itemId = document.getElementById("item_code"+row_no).value;
+            }
+            else if(option==2){
+                itemId = document.getElementById("item_name"+row_no).value;
+            }
+            $.ajax({
+                type: "GET",
+                url: "/items/detail",
+                data: {id:itemId},
+                success: function(result){
+                    $('#item_code'+row_no).val(result[0]['it_cod']);
+                    $('#item_name'+row_no).val(result[0]['it_cod']).select2();
+                    $('#remarks'+row_no).val(result[0]['item_remark']);
+                    $('#price'+row_no).val(result[0]['sales_price']);
+                },
+                error: function(){
+                    alert("error");
+                }
+            });
+            
+        }
+
+        function addNewRow(){		
+            var lastRow =  $('#myTable tr:last');
+            latestValue=lastRow[0].cells[1].querySelector('select').value;
+
+            if(latestValue!="Select Item"){
+                var table = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+                var newRow = table.insertRow(table.rows.length);
+
+                var cell1 = newRow.insertCell(0);
+                var cell2 = newRow.insertCell(1);
+                var cell3 = newRow.insertCell(2);
+                var cell4 = newRow.insertCell(3);
+                var cell5 = newRow.insertCell(4);
+                var cell6 = newRow.insertCell(4);
+
+                cell1.innerHTML = '<input type="text" id="item_code'+index+'" name="item_code[]" onchange="getItemDetails('+index+','+1+')" placeholder="Code" class="form-control" required>';
+                cell2.innerHTML = '<select data-plugin-selecttwo class="form-control select2-js" id="item_name'+index+'" required onchange="getItemDetails('+index+','+2+')" name="item_name">'+
+                                        '<option>Select Item</option>'+
+                                        @foreach($items as $key => $row)	
+                                            '<option value="{{$row->it_cod}}">{{$row->item_name}}</option>'+
+                                        @endforeach
+                                    '</select>';
+                cell3.innerHTML = '<input type="number" id="item_qty'+index+'"  onchange="rowTotal('+index+')" name="item_qty[]" placeholder="Qty" value="0" step="any" required class="form-control">';
+                cell4.innerHTML = '<input type="number" id="price'+index+'" onchange="rowTotal('+index+')" name="item_price[]"  placeholder="Price" value="0" step="any" required class="form-control">';
+                cell5.innerHTML = '<input type="number" id="amount'+index+'" name="item_amount[]" placeholder="Amount" class="form-control" value="0" step="any" required disabled>';
+                cell6.innerHTML = '<button type="button" onclick="removeRow(this)" class="btn btn-danger" tabindex="1"><i class="fas fa-times"></i></button>';
+
+                index++;
+
+                var itemCount = Number($('#itemCount').val());
+                itemCount = itemCount+1;
+                $('#itemCount').val(itemCount);
+                $('#myTable select[data-plugin-selecttwo]').select2();
+
+            }
+	    }
+
+        function rowTotal(index){
+            var weight = $('#weight'+index+'').val();
+            var price = $('#price'+index+'').val();
+            var amount = weight * price;
+            $('#amount'+index+'').val(amount);
+            tableTotal();
+	    }
+
+        function tableTotal(){
+            var totalAmount=0;
+            var totalWeight=0;
+            var totalQuantity=0;
+            var tableRows = $("#saleInvoiceTable tr").length;
+            var table = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+
+            for (var i = 0; i < tableRows; i++) {
+                var currentRow =  table.rows[i];
+                totalAmount = totalAmount + Number(currentRow.cells[6].querySelector('input').value);
+                totalWeight = totalWeight + Number(currentRow.cells[4].querySelector('input').value);
+                totalQuantity = totalQuantity + Number(currentRow.cells[1].querySelector('input').value);
+            }
+
+            $('#totalAmount').val(totalAmount);
+            $('#total_amount_show').val(totalAmount);
+            $('#total_weight').val(totalWeight);
+            $('#total_weight_show').val(totalWeight);
+            $('#total_quantity').val(totalQuantity);
+
+            netTotal();
+        }
+
+        function netTotal(){
+            var netTotal = 0;
+            var total = Number($('#totalAmount').val());
+            var convance_charges = Number($('#convance_charges').val());
+            var labour_charges = Number($('#labour_charges').val());
+            var bill_discount = Number($('#bill_discount').val());
+
+            netTotal = total + convance_charges + labour_charges - bill_discount;
+            netTotal = netTotal.toFixed(0);
+            FormattednetTotal = formatNumberWithCommas(netTotal);
+            document.getElementById("netTotal").innerHTML = '<span class="text-4 text-danger">'+FormattednetTotal+'</span>';
+        }
+
+        function formatNumberWithCommas(number) {
+            // Convert number to string and add commas
+            return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+    </script>
 </html>
