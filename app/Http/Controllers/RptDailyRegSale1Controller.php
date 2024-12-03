cafe6c90b21780570d5edab8dc018cd6447b300c
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-03 18:14:07 +0500

    Update RptDailyRegSale1Controller.php

--- app/Http/Controllers/RptDailyRegSale1Controller.php
+++ app/Http/Controllers/RptDailyRegSale1Controller.php
@@ -122,10 +122,12 @@ class RptDailyRegSale1Controller extends Controller
 
         foreach ($activite5_sales as $items) {
             // Check if a new page is needed
-            if ($pdf->getY() > 250) { // Adjust 250 based on your page margins
+            $currentY = $pdf->GetY();
+            if ($pdf->getY() > 57) { // Adjust 250 based on your page margins
                 $html .= '</table>'; // Close the current table
                 $pdf->writeHTML($html, true, false, true, false, '');
                 $pdf->AddPage(); // Add a new page
+                $currentY = $pdf->GetY()+15;
                 $html = '<table border="1" style="border-collapse: collapse;text-align:center">';
                 $html .= $tableHeader; // Re-add table header
                 $pdf->setTableHtml($tableHeader);
