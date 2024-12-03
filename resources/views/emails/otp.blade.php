f93db08daf289281fa3cfe02b4d3ac0fb459e4d0
myousufmachiyara <m.yousufmachiyara@gmail.com>
2024-12-03 13:34:06 +0000

    updates

---
+++ resources/views/emails/otp.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mail from My Application</title>
+</head>
+<body>
+    <h1>{{ $details['title'] }}</h1>
+    <p>{{ $details['body'] }}</p>
+</body>
+</html>
