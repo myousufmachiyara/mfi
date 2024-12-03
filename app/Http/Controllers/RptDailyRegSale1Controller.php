77eaa7935a6513e768aeaafcaa703d5bd652eec9
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-03 16:04:23 +0500

    Update RptDailyRegSale1Controller.php

--- app/Http/Controllers/RptDailyRegSale1Controller.php
+++ app/Http/Controllers/RptDailyRegSale1Controller.php
@@ -95,7 +95,7 @@ class RptDailyRegSale1Controller extends Controller
         // Report heading
         $html = '<h1 style="font-size:20px;text-align:center; font-style:italic;text-decoration:underline;color:#17365D">Daily Register Sale 1</h1>';
         $pdf->writeHTML($html, true, false, true, false, '');
-       // $pdf->setTableHtml($html);
+        $pdf->setTableHtml($html);
 
         // Header details
         $htmlHeaderDetails = '
