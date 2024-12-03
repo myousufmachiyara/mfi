0e8c700c9c8643931668fe3edef6b5a975d41695
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-03 17:05:53 +0500

    Update RptDailyRegSale1Controller.php

--- app/Http/Controllers/RptDailyRegSale1Controller.php
+++ app/Http/Controllers/RptDailyRegSale1Controller.php
@@ -119,17 +119,19 @@ class RptDailyRegSale1Controller extends Controller
         
         $count = 1;
         $totalAmount = 0;
+
+        // Check if a new page is needed based on remaining space
+        if (($pdf->getPageHeight() - $pdf->GetY()) > 250) { 
+            $pdf->AddPage(); // Add a new page if there's not enough space left
+            $currentY = $pdf->GetY() + 1; // Set the current Y position with a buffer
+            $html .= '</table>'; // Close the current table
+            $pdf->writeHTML($html, true, false, true, false, ''); // Write the table to the PDF
+            $html = '<table border="1" style="border-collapse: collapse;text-align:center">'; // Start a new table
+            $html .= $tableHeader; // Re-add the table header for the new page
+        }
         
         foreach ($activite5_sales as $items) {
-            // Check if a new page is needed based on remaining space
-            if (($pdf->getPageHeight() - $pdf->GetY()) > 250) { 
-                $pdf->AddPage(); // Add a new page if there's not enough space left
-                $currentY = $pdf->GetY() + 1; // Set the current Y position with a buffer
-                $html .= '</table>'; // Close the current table
-                $pdf->writeHTML($html, true, false, true, false, ''); // Write the table to the PDF
-                $html = '<table border="1" style="border-collapse: collapse;text-align:center">'; // Start a new table
-                $html .= $tableHeader; // Re-add the table header for the new page
-            }
+            
         
             // Add table rows
             $bgColor = ($count % 2 == 0) ? '#f1f1f1' : '#ffffff';
