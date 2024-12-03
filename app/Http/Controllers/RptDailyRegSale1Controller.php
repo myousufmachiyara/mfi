58d7c9ddec716e9fd8f0b3d4880bb15dfeffe951
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-03 16:33:48 +0500

    Update RptDailyRegSale1Controller.php

--- app/Http/Controllers/RptDailyRegSale1Controller.php
+++ app/Http/Controllers/RptDailyRegSale1Controller.php
@@ -121,16 +121,16 @@ class RptDailyRegSale1Controller extends Controller
         $totalAmount = 0;
 
         foreach ($activite5_sales as $items) {
-          // Check if a new page is needed
-            if ($pdf->getY() < 250) { // Adjust 250 based on your page margins
+            // Check if a new page is needed based on remaining space
+            if (($pdf->getPageHeight() - $pdf->GetY()) < 57) { 
+                $pdf->AddPage(); // Add a new page if there's not enough space left
+                $currentY = $pdf->GetY() + 15; // Set the current Y position with a buffer
                 $html .= '</table>'; // Close the current table
-                $pdf->writeHTML($html, true, false, true, false, ''); // Write the current table to the PDF
-                $pdf->AddPage(); // Add a new page
+                $pdf->writeHTML($html, true, false, true, false, ''); // Write the table to the PDF
                 $html = '<table border="1" style="border-collapse: collapse;text-align:center">'; // Start a new table
                 $html .= $tableHeader; // Re-add the table header for the new page
             }
-                        
-        
+
 
             // Add table rows
             $bgColor = ($count % 2 == 0) ? '#f1f1f1' : '#ffffff';
