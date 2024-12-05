f3bef2a71845d6775f8709aba117a950ae3c3038
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-05 15:20:36 +0500

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -217,10 +217,9 @@ class UsersController extends Controller
             $user_devices = user_devices::where('user_id', $user->id)
             ->where('device_id', Hash::make($request->browser_id))
             ->first();
-            die($user_devices.'otp'.$request->otp);
 
             // Handle OTP if provided
-            if ($request->has('otp')) {
+            if ($request->has('otp') && $request->otp !== "") {
                 $currentTimestamp = Carbon::now(); // Get current timestamp using Carbon
     
                 $user_otp = login_otps::where('user_id', $user->id)
