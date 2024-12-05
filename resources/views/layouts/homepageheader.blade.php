3ce2d0c9d4d213781ed50e35b74a7967638f13e6
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-05 16:33:57 +0500

    Update homepageheader.blade.php

--- resources/views/layouts/homepageheader.blade.php
+++ resources/views/layouts/homepageheader.blade.php
@@ -87,8 +87,8 @@
 		</div>
 	</div>
 
-	<div id="changePassword" class="zoom-anim-dialog modal-block modal-block-danger mfp-hide">
-		<form id="changePasswordForm" method="post" action="{{ route('change-user-password') }}" enctype="multipart/form-data" onkeydown="return event.key != 'Enter';">
+	<div id="changePassword" class="zoom-anim-dialog modal-block modal-block-danger mfp-hide" >
+		<form id="changePasswordForm" method="post" action="{{ route('change-user-password') }}" style="width: 75%" enctype="multipart/form-data" onkeydown="return event.key != 'Enter';">
 			@csrf
 			<header class="card-header">
 				<h2 class="card-title">Change Password</h2>
