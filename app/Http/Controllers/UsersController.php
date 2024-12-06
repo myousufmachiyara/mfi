d638d7dbf1b2beb71c28de394ac8de05dc8696ac
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-06 16:17:02 +0500

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -209,7 +209,7 @@ class UsersController extends Controller
         $browserVersion = $agent->version($browserName);  // Get the browser version
         $userLocation = $this->getUserLocation();  // Get the browser version
 
-        die($browserVersion);
+        die($userLocation);
 
         // Validate the request
         $request->validate([
