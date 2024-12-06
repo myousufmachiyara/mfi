2994e3e430d3cb749076b820351b84acd85bdadb
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-06 14:42:15 +0500

    Update users.blade.php

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -72,7 +72,7 @@
                                                         <td class="actions">
                                                             <a class="mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal modal-with-form" onclick="getUserDetails({{$row->id}})" href="#updateModal"><i class="fas fa-pencil-alt"></i></a>
                                                             <a class="mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal text-primary" href="#updateCred" onclick="setUserCredID({{$row->id}},'{{$row->username}}')" ><i class="fa fa-user-lock"></i></a>
-                                                            <a class="mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal text-danger" href="#deactivateUser" onclick="setDeactivateID({{$row->id}})"><i class="fa fa-user-minus"></i></a>
+                                                            <a class="mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal text-danger" href="#deactivateUser" onclick="setDeactivateID({{$row->id}})"><i class="fa fa-times"></i></a>
                                                         </td>
                                                         @elseif($row->status==0)
                                                         <td class="actions">
